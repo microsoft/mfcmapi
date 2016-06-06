a7f5fbdad5d6be15a44bda07ab1c28bf7a3cc920
sgriffin_cp <stephenegriffin@users.noreply.github.com>
2016-06-06 03:02:46 +0000

    Found more using to cut

--- Attributes.h
+++ Attributes.h
@@ -1,7 +1,5 @@
 #pragma once
 
-using namespace std;
-
 class Attribute
 {
 public:
--- InterpretProp.h
+++ InterpretProp.h
@@ -1,7 +1,5 @@
 #pragma once
 
-using namespace std;
-
 // Base64 functions
 _Check_return_ HRESULT Base64Decode(_In_z_ LPCTSTR szEncodedStr, _Inout_ size_t* cbBuf, _Out_ _Deref_post_cap_(*cbBuf) LPBYTE* lpDecodedBuffer);
 _Check_return_ HRESULT Base64Encode(size_t cbSourceBuf, _In_count_(cbSourceBuf) LPBYTE lpSourceBuffer, _Inout_ size_t* cchEncodedStr, _Out_ _Deref_post_cap_(*cchEncodedStr) LPTSTR* szEncodedStr);
