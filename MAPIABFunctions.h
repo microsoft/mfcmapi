388ac3737fdafede83b2f1f341f01c9d2bf58b8a
sgriffin_cp <stephenegriffin@users.noreply.github.com>
2016-06-23 14:20:37 +0000

    Add ability to add multiple recipients at a time

--- MAPIABFunctions.h
+++ MAPIABFunctions.h
@@ -3,33 +3,33 @@
 #pragma once
 
 _Check_return_ HRESULT AddOneOffAddress(
-										_In_ LPMAPISESSION lpMAPISession,
-										_In_ LPMESSAGE lpMessage,
-										_In_z_ LPCTSTR szDisplayName,
-										_In_z_ LPCTSTR szAddrType,
-										_In_z_ LPCTSTR szEmailAddress,
-										ULONG ulRecipientType);
+	_In_ LPMAPISESSION lpMAPISession,
+	_In_ LPMESSAGE lpMessage,
+	_In_ wstring& szDisplayName,
+	_In_ wstring& szAddrType,
+	_In_ wstring& szEmailAddress,
+	ULONG ulRecipientType);
 
 _Check_return_ HRESULT AddRecipient(
-									_In_ LPMAPISESSION lpMAPISession,
-									_In_ LPMESSAGE lpMessage,
-									_In_z_ LPCTSTR szName,
-									ULONG ulRecipientType);
+	_In_ LPMAPISESSION lpMAPISession,
+	_In_ LPMESSAGE lpMessage,
+	_In_z_ LPCTSTR szName,
+	ULONG ulRecipientType);
 
 _Check_return_ HRESULT CreateANRRestriction(ULONG ulPropTag,
-											_In_z_ LPCTSTR szString,
-											_In_opt_ LPVOID lpParent,
-											_Deref_out_opt_ LPSRestriction* lppRes);
+	_In_z_ LPCTSTR szString,
+	_In_opt_ LPVOID lpParent,
+	_Deref_out_opt_ LPSRestriction* lppRes);
 
 _Check_return_ HRESULT GetABContainerTable(_In_ LPADRBOOK lpAdrBook, _Deref_out_opt_ LPMAPITABLE* lpABContainerTable);
 
 _Check_return_ HRESULT HrAllocAdrList(ULONG ulNumProps, _Deref_out_opt_ LPADRLIST* lpAdrList);
 
 _Check_return_ HRESULT ManualResolve(
-									 _In_ LPMAPISESSION lpMAPISession,
-									 _In_ LPMESSAGE lpMessage,
-									 _In_z_ LPCTSTR szName,
-									 ULONG PropTagToCompare);
+	_In_ LPMAPISESSION lpMAPISession,
+	_In_ LPMESSAGE lpMessage,
+	_In_z_ LPCTSTR szName,
+	ULONG PropTagToCompare);
 
 _Check_return_ HRESULT SearchContentsTableForName(
 	_In_ LPMAPITABLE pTable,
