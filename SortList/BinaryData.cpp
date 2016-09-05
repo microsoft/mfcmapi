1bddcac06431ec54036edbec0a5d8068b295c79d
sgriffin_cp <stephenegriffin@users.noreply.github.com>
2016-09-05 20:39:55 +0000

    Factor out BinaryData

---
+++ SortList/BinaryData.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include "BinaryData.h"
+
+BinaryData::BinaryData(_In_ LPSBinary lpOldBin)
+{
+	m_OldBin = { 0 };
+	if (lpOldBin)
+	{
+		m_OldBin.cb = lpOldBin->cb;
+		m_OldBin.lpb = lpOldBin->lpb;
+	}
+
+	m_NewBin = { 0 };
+}
+
+BinaryData::~BinaryData()
+{
+
+}
---
+++ SortList/CommentData.cpp
@@ -0,0 +1,8 @@
+﻿#include "stdafx.h"
+#include "CommentData.h"
+
+CommentData::CommentData(_In_ LPSPropValue lpOldProp)
+{
+	m_lpOldProp = lpOldProp;
+	m_lpNewProp = nullptr;
+}
