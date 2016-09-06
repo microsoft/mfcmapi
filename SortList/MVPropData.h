a8d28fe1d6e4aec7e4e9f5b91a9370a0bc09b9cb
sgriffin_cp <SND\sgriffin_cp>
2016-09-06 02:00:07 +0000

    rename members of MVPropData

--- SortList/MVPropData.h
+++ SortList/MVPropData.h
@@ -6,9 +6,9 @@ class MVPropData
 public:
 	MVPropData(_In_ LPSPropValue lpProp, ULONG iProp);
 	MVPropData(_In_ LPSPropValue lpNewValue);
-	_PV m_val; // Stolen from input prop
+	_PV m_val;
 private:
-	string lpszA;
-	wstring lpszW;
-	vector<BYTE> lpBin;
+	string m_lpszA;
+	wstring m_lpszW;
+	vector<BYTE> m_lpBin;
 };
