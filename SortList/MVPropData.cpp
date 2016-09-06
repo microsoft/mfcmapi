8306bf4b0b3f8de03325e2b340daa1e5dc9670cb
sgriffin_cp <stephenegriffin@users.noreply.github.com>
2016-09-06 02:00:07 +0000

    rename members of MVPropData

--- SortList/MVPropData.cpp
+++ SortList/MVPropData.cpp
@@ -33,23 +33,23 @@ MVPropData::MVPropData(_In_ LPSPropValue lpProp, ULONG iProp)
 		m_val.flt = lpProp->Value.MVflt.lpflt[iProp];
 		break;
 	case PT_MV_STRING8:
-		lpszA = lpProp->Value.MVszA.lppszA[iProp];
-		m_val.lpszA = const_cast<LPSTR>(lpszA.c_str());
+		m_lpszA = lpProp->Value.MVszA.lppszA[iProp];
+		m_val.lpszA = const_cast<LPSTR>(m_lpszA.c_str());
 		break;
 	case PT_MV_UNICODE:
-		lpszW = lpProp->Value.MVszW.lppszW[iProp];
-		m_val.lpszW = const_cast<LPWSTR>(lpszW.c_str());
+		m_lpszW = lpProp->Value.MVszW.lppszW[iProp];
+		m_val.lpszW = const_cast<LPWSTR>(m_lpszW.c_str());
 		break;
 	case PT_MV_BINARY:
-		lpBin.assign(lpProp->Value.MVbin.lpbin[iProp].lpb, lpProp->Value.MVbin.lpbin[iProp].lpb + lpProp->Value.MVbin.lpbin[iProp].cb);
-		m_val.bin.cb = static_cast<ULONG>(lpBin.size());
-		m_val.bin.lpb = lpBin.data();
+		m_lpBin.assign(lpProp->Value.MVbin.lpbin[iProp].lpb, lpProp->Value.MVbin.lpbin[iProp].lpb + lpProp->Value.MVbin.lpbin[iProp].cb);
+		m_val.bin.cb = static_cast<ULONG>(m_lpBin.size());
+		m_val.bin.lpb = m_lpBin.data();
 		break;
 	case PT_MV_CLSID:
-		lpBin.assign(
+		m_lpBin.assign(
 			reinterpret_cast<LPBYTE>(&lpProp->Value.MVguid.lpguid[iProp]),
 			reinterpret_cast<LPBYTE>(&lpProp->Value.MVguid.lpguid[iProp]) + sizeof(GUID));
-		m_val.lpguid = reinterpret_cast<LPGUID>(lpBin.data());
+		m_val.lpguid = reinterpret_cast<LPGUID>(m_lpBin.data());
 		break;
 	default:
 		break;
@@ -67,23 +67,23 @@ MVPropData::MVPropData(_In_ LPSPropValue lpProp)
 	switch (PROP_TYPE(lpProp->ulPropTag))
 	{
 	case PT_STRING8:
-		lpszA = lpProp->Value.lpszA;
-		m_val.lpszA = const_cast<LPSTR>(lpszA.c_str());
+		m_lpszA = lpProp->Value.lpszA;
+		m_val.lpszA = const_cast<LPSTR>(m_lpszA.c_str());
 		break;
 	case PT_UNICODE:
-		lpszW = lpProp->Value.lpszW;
-		m_val.lpszW = const_cast<LPWSTR>(lpszW.c_str());
+		m_lpszW = lpProp->Value.lpszW;
+		m_val.lpszW = const_cast<LPWSTR>(m_lpszW.c_str());
 		break;
 	case PT_BINARY:
-		lpBin.assign(m_val.bin.lpb, m_val.bin.lpb + lpProp->Value.bin.cb);
-		m_val.bin.cb = static_cast<ULONG>(lpBin.size());
-		m_val.bin.lpb = lpBin.data();
+		m_lpBin.assign(m_val.bin.lpb, m_val.bin.lpb + lpProp->Value.bin.cb);
+		m_val.bin.cb = static_cast<ULONG>(m_lpBin.size());
+		m_val.bin.lpb = m_lpBin.data();
 		break;
 	case PT_CLSID:
-		lpBin.assign(
+		m_lpBin.assign(
 			reinterpret_cast<LPBYTE>(lpProp->Value.lpguid),
 			reinterpret_cast<LPBYTE>(lpProp->Value.lpguid) + sizeof(GUID));
-		m_val.lpguid = reinterpret_cast<LPGUID>(lpBin.data());
+		m_val.lpguid = reinterpret_cast<LPGUID>(m_lpBin.data());
 		break;
 	default:
 		break;
