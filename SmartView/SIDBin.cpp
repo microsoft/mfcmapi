39c76cfee28368250666561c1fafae607bb19aa8
sgriffin_cp <SND\sgriffin_cp>
2016-10-02 02:42:31 +0000

    Remove account sid TCHARs

--- SmartView/SIDBin.cpp
+++ SmartView/SIDBin.cpp
@@ -1,13 +1,12 @@
 #include "stdafx.h"
-#include "..\stdafx.h"
 #include "SIDBin.h"
-#include "..\String.h"
-#include "..\MySecInfo.h"
+#include "String.h"
+#include "MySecInfo.h"
 
 SIDBin::SIDBin(ULONG cbBin, _In_count_(cbBin) LPBYTE lpBin) : SmartViewParser(cbBin, lpBin)
 {
-	m_lpSidName = NULL;
-	m_lpSidDomain = NULL;
+	m_lpSidName = nullptr;
+	m_lpSidDomain = nullptr;
 }
 
 SIDBin::~SIDBin()
@@ -18,29 +17,29 @@ SIDBin::~SIDBin()
 
 void SIDBin::Parse()
 {
-	HRESULT hRes = S_OK;
+	auto hRes = S_OK;
 	PSID SidStart = m_Parser.GetCurrentAddress();
-	size_t cbSid = m_Parser.RemainingBytes();
+	auto cbSid = m_Parser.RemainingBytes();
 	m_Parser.Advance(cbSid);
 
 	if (SidStart &&
-		cbSid >= sizeof(SID) - sizeof(DWORD) + sizeof(DWORD)* ((PISID)SidStart)->SubAuthorityCount &&
+		cbSid >= sizeof(SID) - sizeof(DWORD) + sizeof(DWORD)* static_cast<PISID>(SidStart)->SubAuthorityCount &&
 		IsValidSid(SidStart))
 	{
 		DWORD dwSidName = 0;
 		DWORD dwSidDomain = 0;
 		SID_NAME_USE SidNameUse;
 
-		if (!LookupAccountSid(
-			NULL,
+		if (!LookupAccountSidW(
+			nullptr,
 			SidStart,
-			NULL,
+			nullptr,
 			&dwSidName,
-			NULL,
+			nullptr,
 			&dwSidDomain,
 			&SidNameUse))
 		{
-			DWORD dwErr = GetLastError();
+			auto dwErr = GetLastError();
 			hRes = HRESULT_FROM_WIN32(dwErr);
 			if (ERROR_NONE_MAPPED != dwErr &&
 				STRSAFE_E_INSUFFICIENT_BUFFER != hRes)
@@ -51,16 +50,13 @@ void SIDBin::Parse()
 			hRes = S_OK;
 		}
 
-#pragma warning(push)
-#pragma warning(disable:6211)
-		if (dwSidName) m_lpSidName = new TCHAR[dwSidName];
-		if (dwSidDomain) m_lpSidDomain = new TCHAR[dwSidDomain];
-#pragma warning(pop)
+		if (dwSidName) m_lpSidName = new WCHAR[dwSidName];
+		if (dwSidDomain) m_lpSidDomain = new WCHAR[dwSidDomain];
 
 		// Only make the call if we got something to get
 		if (m_lpSidName || m_lpSidDomain)
 		{
-			WC_B(LookupAccountSid(
+			WC_B(LookupAccountSidW(
 				NULL,
 				SidStart,
 				m_lpSidName,
@@ -68,7 +64,6 @@ void SIDBin::Parse()
 				m_lpSidDomain,
 				&dwSidDomain,
 				&SidNameUse));
-			hRes = S_OK;
 		}
 
 		m_lpStringSid = GetTextualSid(SidStart);
@@ -77,9 +72,9 @@ void SIDBin::Parse()
 
 _Check_return_ wstring SIDBin::ToStringInternal()
 {
-	wstring szDomain = m_lpSidDomain ? LPCTSTRToWstring(m_lpSidDomain) : formatmessage(IDS_NODOMAIN);
-	wstring szName = m_lpSidName ? LPCTSTRToWstring(m_lpSidName) : formatmessage(IDS_NONAME);
-	wstring szSID = !m_lpStringSid.empty() ? m_lpStringSid : formatmessage(IDS_NOSID);
+	auto szDomain = m_lpSidDomain ? m_lpSidDomain : formatmessage(IDS_NODOMAIN);
+	auto szName = m_lpSidName ? m_lpSidName : formatmessage(IDS_NONAME);
+	auto szSID = !m_lpStringSid.empty() ? m_lpStringSid : formatmessage(IDS_NOSID);
 
 	return formatmessage(IDS_SIDHEADER, szDomain.c_str(), szName.c_str(), szSID.c_str());
 }
