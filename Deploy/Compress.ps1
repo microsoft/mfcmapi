f998375271fb9e12391881123bb8254d9c4d8c87
sgriffin <sgriffin@outlook.com>
2016-10-16 02:43:55 -0400

    Update paths in compress script

--- Deploy/Compress.ps1
+++ Deploy/Compress.ps1
@@ -1,22 +1,19 @@
+$indir = "$Env:SYSTEM_ARTIFACTSDIRECTORY"
 $outdir = "$Env:SYSTEM_ARTIFACTSDIRECTORY\out"
 $version = $Env:BUILD_BUILDNUMBER
-$release = $($args[0])
-$username = $($args[1])
-$password = $($args[2])
 
-Write-Host "Release=$release"
-Write-Host "Username=$username"
-Write-Host "Password=$password"
+Write-Host "indir=$indir"
+Write-Host "outdir=$outdir"
 Write-Host "Version=$version"
 
 if (Test-Path $outdir) {Remove-Item -Path $outdir -Recurse}
 if (!(Test-Path $outdir)) {New-Item -Path $outdir -Type Directory}
 
-Compress-Archive .\MrMAPI\Win32\MrMAPI.exe $outdir\MrMAPI.exe.$version.zip
-Compress-Archive .\MrMAPI\Win32\MrMAPI.pdb $outdir\MrMAPI.pdb.$version.zip
-Compress-Archive .\MrMAPI\x64\MrMAPI.exe $outdir\MrMAPI.exe.x64.$version.zip
-Compress-Archive .\MrMAPI\x64\MrMAPI.exe $outdir\MrMAPI.pdb.x64.$version.zip
-Compress-Archive .\Release\Win32\MFCMAPI.exe $outdir\MFCMAPI.exe.$version.zip
-Compress-Archive .\Release\Win32\MFCMAPI.pdb $outdir\MFCMAPI.pdb.$version.zip
-Compress-Archive .\Release\x64\MFCMAPI.exe $outdir\MFCMAPI.exe.x64.$version.zip
-Compress-Archive .\Release\x64\MFCMAPI.exe $outdir\MFCMAPI.pdb.x64.$version.zip
+Compress-Archive "$indir\MrMAPI\Win32\MrMAPI.exe" "$outdir\MrMAPI.exe.$version.zip"
+Compress-Archive "$indir\MrMAPI\Win32\MrMAPI.pdb" "$outdir\MrMAPI.pdb.$version.zip"
+Compress-Archive "$indir\MrMAPI\x64\MrMAPI.exe" "$outdir\MrMAPI.exe.x64.$version.zip"
+Compress-Archive "$indir\MrMAPI\x64\MrMAPI.exe" "$outdir\MrMAPI.pdb.x64.$version.zip"
+Compress-Archive "$indir\Release\Win32\MFCMAPI.exe" "$outdir\MFCMAPI.exe.$version.zip"
+Compress-Archive "$indir\Release\Win32\MFCMAPI.pdb" "$outdir\MFCMAPI.pdb.$version.zip"
+Compress-Archive "$indir\Release\x64\MFCMAPI.exe" "$outdir\MFCMAPI.exe.x64.$version.zip"
+Compress-Archive "$indir\Release\x64\MFCMAPI.exe" "$outdir\MFCMAPI.pdb.x64.$version.zip"
