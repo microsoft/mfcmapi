8a94887df5927117658f14ef1a96530cb038726a
Stephen Griffin <sgriffin@outlook.com>
2017-04-10 12:37:45 -0400

    cleanup script

--- Deploy/Stats.ps1
+++ Deploy/Stats.ps1
@@ -1,36 +1,34 @@
 $gitHubUsername = 'stephenegriffin'
 $gitHubRepository = 'mfcmapi'
-$project = "MFCMAPI"
 
 function Stats-Release {
-  <#
-  .SYNOPSIS
-  Gets stats for a release
-  .DESCRIPTION
-  Describe the function in more detail
-  .EXAMPLE
-  Stats-Release 
-  #>
-  [CmdletBinding()]
-  param
-  (
-  )
-  process
-  {
+	<#
+	.SYNOPSIS
+	Gets stats for a release
+	.DESCRIPTION
+	Describe the function in more detail
+	.EXAMPLE
+	Stats-Release 
+	#>
+	[CmdletBinding()]
+	param
+	(
+	)
+	process
+	{
 	Write-Host "Getting stats"
 
 	$releaseStats = @{
-	   Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/releases/latest";
-	   Method = 'GET';
-	   ContentType = 'application/json';
+		Uri = "https://api.github.com/repos/$gitHubUsername/$gitHubRepository/releases/latest";
+		Method = 'GET';
+		ContentType = 'application/json';
 	}
 
-	$result = Invoke-RestMethod @releaseStats 
+	$result = Invoke-RestMethod @releaseStats
 
 	return $result
-  }
+	}
 }
 
 $stats = Stats-Release
 $stats.assets |fl name,download_count
-
