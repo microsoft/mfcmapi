319795113fd60c98d49b29fd1d869a40276fca08
Stephen Griffin <sgriffin@outlook.com>
2018-04-29 11:10:36 -0400

    Remove redundant framework code

--- UnitTest/SmartViewTest.cpp
+++ UnitTest/SmartViewTest.cpp
@@ -38,15 +38,3 @@ namespace SmartViewTest
 		}
 	};
 }
-
-namespace Microsoft
-{
-	namespace VisualStudio
-	{
-		namespace CppUnitTestFramework
-		{
-			template<> inline std::wstring ToString<__int64>(const __int64& t) { RETURN_WIDE_STRING(t); }
-			template<> inline std::wstring ToString<vector<BYTE>>(const vector<BYTE>& t) { RETURN_WIDE_STRING(t.data()); }
-		}
-	}
-}
