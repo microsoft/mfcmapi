258e1f21a1d7204dd7448eae701acf73a304b6df
Stephen Griffin <sgriffin@outlook.com>
2018-04-29 11:18:17 -0400

    Move framework code up to stdafx.h

--- UnitTest/stringtest.cpp
+++ UnitTest/stringtest.cpp
@@ -335,15 +335,3 @@ namespace stringtest
 		}
 	};
 }
-
-namespace Microsoft
-{
-	namespace VisualStudio
-	{
-		namespace CppUnitTestFramework
-		{
-			template<> inline std::wstring ToString<__int64>(const __int64& t) { RETURN_WIDE_STRING(t); }
-			template<> inline std::wstring ToString<vector<BYTE>>(const vector<BYTE>& t) { RETURN_WIDE_STRING(t.data()); }
-		}
-	}
-}
