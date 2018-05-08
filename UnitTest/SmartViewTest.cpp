438ee38f21ce1f8de638aaff5bb658f3b3eb6ac5
sgriffin <sgriffin@microsoft.com>
2018-05-08 10:20:53 -0400

    Pull AreEqualEx back into SmartViewTest

--- UnitTest/SmartViewTest.cpp
+++ UnitTest/SmartViewTest.cpp
@@ -27,7 +27,47 @@ namespace SmartViewTest
 			wstring expected;
 		};
 
-		const bool parseAll = false;
+		static const bool parseAll = false;
+		static const bool AssertOnFailure = true;
+		static const bool LimitOutput = false;
+
+		// Assert::AreEqual doesn't do full logging, so we roll our own
+		void AreEqualEx(const wstring& expected, const wstring& actual, const wchar_t* message = nullptr, const __LineInfo* pLineInfo = nullptr)
+		{
+			if (expected != actual)
+			{
+				if (message != nullptr)
+				{
+					Logger::WriteMessage(format(L"Test: %ws\n", message).c_str());
+				}
+
+				Logger::WriteMessage(L"Diff:\n");
+
+				auto splitExpected = split(expected, L'\n');
+				auto splitActual = split(actual, L'\n');
+				auto errorCount = 0;
+				for (size_t i = 0; i < splitExpected.size() && i < splitActual.size() && (errorCount < 4 || !LimitOutput); i++)
+				{
+					if (splitExpected[i] != splitActual[i])
+					{
+						errorCount++;
+						Logger::WriteMessage(format(L"[%d]\n\"%ws\"\n\"%ws\"\n", i, splitExpected[i].c_str(), splitActual[i].c_str()).c_str());
+					}
+				}
+
+				if (!LimitOutput)
+				{
+					Logger::WriteMessage(L"\n");
+					Logger::WriteMessage(format(L"Expected:\n\"%ws\"\n\n", expected.c_str()).c_str());
+					Logger::WriteMessage(format(L"Actual:\n\"%ws\"", actual.c_str()).c_str());
+				}
+
+				if (AssertOnFailure)
+				{
+					Assert::Fail(ToString(message).c_str(), pLineInfo);
+				}
+			}
+		}
 
 		void test(vector<SmartViewTestData> testData)
 		{
--- UnitTest/stdafx.h
+++ UnitTest/stdafx.h
@@ -10,48 +10,8 @@ namespace Microsoft
 	{
 		namespace CppUnitTestFramework
 		{
-			static const bool AssertOnFailure = true;
-			static const bool LimitOutput = false;
 			template<> inline std::wstring ToString<__int64>(const __int64& t) { RETURN_WIDE_STRING(t); }
 			template<> inline std::wstring ToString<vector<BYTE>>(const vector<BYTE>& t) { RETURN_WIDE_STRING(t.data()); }
-
-			// Assert::AreEqual doesn't do full logging, so we roll our own
-			void inline AreEqualEx(const wstring& expected, const wstring& actual, const wchar_t* message = nullptr, const __LineInfo* pLineInfo = nullptr)
-			{
-				if (expected != actual)
-				{
-					if (message != nullptr)
-					{
-						Logger::WriteMessage(format(L"Test: %ws\n", message).c_str());
-					}
-
-					Logger::WriteMessage(L"Diff:\n");
-
-					auto splitExpected = split(expected, L'\n');
-					auto splitActual = split(actual, L'\n');
-					auto errorCount = 0;
-					for (size_t i = 0; i < splitExpected.size() && i < splitActual.size() && (errorCount < 4 || !LimitOutput); i++)
-					{
-						if (splitExpected[i] != splitActual[i])
-						{
-							errorCount++;
-							Logger::WriteMessage(format(L"[%d]\n\"%ws\"\n\"%ws\"\n", i, splitExpected[i].c_str(), splitActual[i].c_str()).c_str());
-						}
-					}
-
-					if (!LimitOutput)
-					{
-						Logger::WriteMessage(L"\n");
-						Logger::WriteMessage(format(L"Expected:\n\"%ws\"\n\n", expected.c_str()).c_str());
-						Logger::WriteMessage(format(L"Actual:\n\"%ws\"", actual.c_str()).c_str());
-					}
-
-					if (AssertOnFailure)
-					{
-						Assert::Fail(ToString(message).c_str(), pLineInfo);
-					}
-				}
-			}
 		}
 	}
 }
