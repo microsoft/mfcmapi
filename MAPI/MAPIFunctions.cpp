e6e383e3cfee7454ffa02992fe4a6a534e2767b8
sgriffin <sgriffin@microsoft.com>
2018-06-04 11:30:08 -0400

    fix comment

--- MAPI/MAPIFunctions.cpp
+++ MAPI/MAPIFunctions.cpp
@@ -1912,7 +1912,7 @@ namespace mapi
 		return hRes;
 	}
 
-	// This function creates a new message based in m_lpContainer
+	// This function creates a new message based in lpFolder
 	// Then sends the message
 	_Check_return_ HRESULT SendTestMessage(
 		_In_ LPMAPISESSION lpMAPISession,
