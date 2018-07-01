8fe69e802f1a14be8b1635d9c2ba1c877d257817
Stephen Griffin <sgriffin@outlook.com>
2018-07-01 13:32:57 -0400

    update mapi macros

--- IO/Error.h
+++ IO/Error.h
@@ -114,6 +114,25 @@ namespace error
 		} \
 	}
 
+// Execute a function, log and return the HRESULT
+// Logs a MAPI call trace under DBGMAPIFunctions
+// Does not modify or reference existing hRes
+#define EC_MAPI2(fnx) \
+	[&]() -> HRESULT { \
+		auto __hRes = (fnx); \
+		error::LogFunctionCall(__hRes, NULL, true, true, false, NULL, #fnx, __FILE__, __LINE__); \
+		return __hRes; \
+	}()
+
+// Execute a function, log and swallow the HRESULT
+// Logs a MAPI call trace under DBGMAPIFunctions
+// Does not modify or reference existing hRes
+#define EC_MAPI2S(fnx) \
+	[&]() -> void { \
+		auto __hRes = (fnx); \
+		error::LogFunctionCall(__hRes, NULL, true, true, false, NULL, #fnx, __FILE__, __LINE__); \
+	}()
+
 #define EC_MAPI(fnx) \
 	{ \
 		if (SUCCEEDED(hRes)) \
@@ -126,6 +145,27 @@ namespace error
 			error::PrintSkipNote(hRes, #fnx); \
 		} \
 	}
+
+// Execute a function, log and return the HRESULT
+// Logs a MAPI call trace under DBGMAPIFunctions
+// Does not modify or reference existing hRes
+// Will not display an error dialog
+#define WC_MAPI2(fnx) \
+	[&]() -> HRESULT { \
+		auto __hRes = (fnx); \
+		error::LogFunctionCall(__hRes, NULL, false, true, false, NULL, #fnx, __FILE__, __LINE__); \
+		return __hRes; \
+	}()
+
+// Execute a function, log and swallow the HRESULT
+// Logs a MAPI call trace under DBGMAPIFunctions
+// Does not modify or reference existing hRes
+// Will not display an error dialog
+#define WC_MAPI2S(fnx) \
+	[&]() -> void { \
+		auto __hRes = (fnx); \
+		error::LogFunctionCall(__hRes, NULL, false, true, false, NULL, #fnx, __FILE__, __LINE__); \
+	}()
 
 #define WC_MAPI(fnx) \
 	{ \
