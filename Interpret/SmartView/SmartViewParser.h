aab2eb52ce39c6c09659265d8f07f2efad218591
Stephen Griffin <sgriffin@outlook.com>
2018-08-02 22:44:15 -0400

    add notion of child blocks

--- Interpret/SmartView/SmartViewParser.h
+++ Interpret/SmartView/SmartViewParser.h
@@ -14,13 +14,26 @@ namespace smartview
 	class block
 	{
 	public:
+		block() : header(true), offset(0), cb(0), text(L"") {}
 		block(size_t _offset, size_t _cb, std::wstring _text) : header(false), offset(_offset), cb(_cb), text(_text) {}
 		block(std::wstring _text) : header(true), offset(0), cb(0), text(_text) {}
-		std::wstring ToString() { return text; }
+		std::wstring ToString()
+		{
+			std::vector<std::wstring> items;
+			items.emplace_back(text);
+			for (auto item : children)
+			{
+				items.emplace_back(item.ToString());
+			}
+
+			return strings::join(items, strings::emptystring);
+		}
 		size_t GetSize() { return cb; }
 		size_t GetOffset() { return offset; }
+		void AddChild(block child) { children.push_back(child); }
 
 	private:
+		std::vector<block> children;
 		bool header;
 		size_t offset;
 		size_t cb;
@@ -59,33 +72,26 @@ namespace smartview
 		CBinaryParser m_Parser;
 
 		// Nu style parsing data
-		std::vector<block> data;
+		block data;
 		size_t dataOffset;
 
-		std::wstring dataToString()
-		{
-			std::vector<std::wstring> items;
-			for (auto item : data)
-			{
-				items.emplace_back(item.ToString());
-			}
+		std::wstring dataToString() { return data.ToString(); }
 
-			return strings::join(items, strings::emptystring);
-		}
-
-		void addHeader(std::wstring text) { data.push_back(block(text)); }
+		void addHeader(std::wstring text) { data.AddChild(block(text)); }
 		void addData(size_t cb, std::wstring text)
 		{
-			data.emplace_back(dataOffset, cb, text);
+			data.AddChild(block(dataOffset, cb, text));
 			dataOffset += cb;
 		}
 
-		void addBytes(std::vector<BYTE> bytes, bool bPrependCB = true)
+		void addBytes(const std::vector<BYTE>& bytes, bool bPrependCB = true)
 		{
 			const auto cb = bytes.size() * sizeof(BYTE);
-			data.emplace_back(dataOffset, cb, strings::BinToHexString(bytes, bPrependCB));
+			data.AddChild(block(dataOffset, cb, strings::BinToHexString(bytes, bPrependCB)));
 			dataOffset += cb;
 		}
+
+		void addBlock(block& _block) { data.AddChild(_block); }
 
 	private:
 		virtual void Parse() = 0;
