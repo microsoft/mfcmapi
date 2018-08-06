0d0212dd7859b1c1614150eac27214f3ca397f98
sgriffin <sgriffin@microsoft.com>
2018-08-06 14:18:38 -0400

    Missed a few getData calls

--- Interpret/SmartView/RuleCondition.cpp
+++ Interpret/SmartView/RuleCondition.cpp
@@ -60,38 +60,38 @@ namespace smartview
 		if (m_bExtended)
 		{
 			ruleCondition.push_back(
-				strings::formatmessage(IDS_EXRULECONHEADER, m_NamedPropertyInformation.NoOfNamedProps));
+				strings::formatmessage(IDS_EXRULECONHEADER, m_NamedPropertyInformation.NoOfNamedProps.getData()));
 		}
 		else
 		{
 			ruleCondition.push_back(
-				strings::formatmessage(IDS_RULECONHEADER, m_NamedPropertyInformation.NoOfNamedProps));
+				strings::formatmessage(IDS_RULECONHEADER, m_NamedPropertyInformation.NoOfNamedProps.getData()));
 		}
 
 		if (m_NamedPropertyInformation.PropId.size())
 		{
-			ruleCondition.push_back(
-				strings::formatmessage(IDS_RULECONNAMEPROPSIZE, m_NamedPropertyInformation.NamedPropertiesSize));
+			ruleCondition.push_back(strings::formatmessage(
+				IDS_RULECONNAMEPROPSIZE, m_NamedPropertyInformation.NamedPropertiesSize.getData()));
 
 			for (size_t i = 0; i < m_NamedPropertyInformation.PropId.size(); i++)
 			{
 				ruleCondition.push_back(
 					strings::formatmessage(IDS_RULECONNAMEPROPID, i, m_NamedPropertyInformation.PropId[i].getData()));
 
-				ruleCondition.push_back(
-					strings::formatmessage(IDS_RULECONNAMEPROPKIND, m_NamedPropertyInformation.PropertyName[i].Kind));
+				ruleCondition.push_back(strings::formatmessage(
+					IDS_RULECONNAMEPROPKIND, m_NamedPropertyInformation.PropertyName[i].Kind.getData()));
 
-				ruleCondition.push_back(guid::GUIDToString(&m_NamedPropertyInformation.PropertyName[i].Guid.getData()));
+				ruleCondition.push_back(guid::GUIDToString(m_NamedPropertyInformation.PropertyName[i].Guid.getData()));
 
 				if (m_NamedPropertyInformation.PropertyName[i].Kind.getData() == MNID_ID)
 				{
-					ruleCondition.push_back(
-						strings::formatmessage(IDS_RULECONNAMEPROPLID, m_NamedPropertyInformation.PropertyName[i].LID));
+					ruleCondition.push_back(strings::formatmessage(
+						IDS_RULECONNAMEPROPLID, m_NamedPropertyInformation.PropertyName[i].LID.getData()));
 				}
 				else if (m_NamedPropertyInformation.PropertyName[i].Kind.getData() == MNID_STRING)
 				{
 					ruleCondition.push_back(strings::formatmessage(
-						IDS_RULENAMEPROPSIZE, m_NamedPropertyInformation.PropertyName[i].NameSize));
+						IDS_RULENAMEPROPSIZE, m_NamedPropertyInformation.PropertyName[i].NameSize.getData()));
 					ruleCondition.push_back(m_NamedPropertyInformation.PropertyName[i].Name.getData());
 				}
 			}
