321f8f07a4f4fd7adbc4c8303e9f31f42bd2f02b
Stephen Griffin <sgriffin@outlook.com>
2018-09-05 21:50:38 -0400

    Minor tweak to flat entry lists

--- Interpret/SmartView/FlatEntryList.h
+++ Interpret/SmartView/FlatEntryList.h
@@ -4,14 +4,18 @@
 
 namespace smartview
 {
+	// FlatEntryID
+	// https://docs.microsoft.com/en-us/office/client-developer/outlook/mapi/flatentry
 	struct FlatEntryID
 	{
 		DWORD dwSize;
 		EntryIdStruct lpEntryID;
 
-		std::vector<BYTE> JunkData;
+		std::vector<BYTE> m_Padding; // 0-3 bytes
 	};
 
+	// FlatEntryList
+	// https://docs.microsoft.com/en-us/office/client-developer/outlook/mapi/flatentrylist
 	class FlatEntryList : public SmartViewParser
 	{
 	public:
@@ -21,8 +25,8 @@ namespace smartview
 		void Parse() override;
 		_Check_return_ std::wstring ToStringInternal() override;
 
-		DWORD m_cEntries;
-		DWORD m_cbEntries;
+		DWORD m_cEntries{};
+		DWORD m_cbEntries{};
 		std::vector<FlatEntryID> m_pEntryIDs;
 	};
 }
