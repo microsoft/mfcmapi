eac6e94ad6b7a63ac8859c030d600aa1ec9ac808
Stephen Griffin <sgriffin@outlook.com>
2018-09-09 14:56:45 -0400

    Remove block.cpp

--- Interpret/SmartView/block/block.cpp
+++
@@ -1,8 +0,0 @@
-#include <StdAfx.h>
-#include <Interpret/SmartView/block/block.h>
-#include <Interpret/SmartView/block/blockBytes.h>
-#include <Interpret/String.h>
-
-namespace smartview
-{
-} // namespace smartview
