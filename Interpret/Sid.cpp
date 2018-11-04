6df1250aba1d31322f4c9b41887f6c19c77772c1
Stephen Griffin <sgriffin@outlook.com>
2018-11-04 00:53:51 -0400

    Further simplify sids

--- Interpret/Sid.cpp
+++ Interpret/Sid.cpp
@@ -159,8 +159,8 @@ namespace sid
 
 		aceString.push_back(strings::formatmessage(
 			IDS_SIDACCOUNT,
-			sidAccount.getDomain().c_str(),
-			sidAccount.getName().c_str(),
+			sidAccount.domain.c_str(),
+			sidAccount.name.c_str(),
 			szSID.c_str(),
 			AceType,
 			szAceType.c_str(),
