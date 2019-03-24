7dfbdd7d5376d2e373dc3ed5b3b7f98d4ac40b4c
Stephen Griffin <sgriffin@outlook.com>
2019-03-24 12:07:37 -0400

    Convert ReportTag

--- core/smartview/ReportTag.cpp
+++ core/smartview/ReportTag.cpp
@@ -12,32 +12,32 @@ namespace smartview
 		// Version is big endian, so we have to read individual bytes
 		const auto& hiWord = blockT<WORD>(m_Parser);
 		const auto& loWord = blockT<WORD>(m_Parser);
-		m_Version.setOffset(hiWord.getOffset());
-		m_Version.setSize(hiWord.getSize() + loWord.getSize());
-		m_Version.setData(hiWord << 16 | loWord);
+		m_Version->setOffset(hiWord.getOffset());
+		m_Version->setSize(hiWord.getSize() + loWord.getSize());
+		m_Version->setData(hiWord << 16 | loWord);
 
-		m_cbStoreEntryID.parse<DWORD>(m_Parser);
-		m_lpStoreEntryID = blockBytes::parse(m_Parser, m_cbStoreEntryID, _MaxEID);
+		m_cbStoreEntryID = blockT<DWORD>::parse(m_Parser);
+		m_lpStoreEntryID = blockBytes::parse(m_Parser, *m_cbStoreEntryID, _MaxEID);
 
-		m_cbFolderEntryID.parse<DWORD>(m_Parser);
-		m_lpFolderEntryID = blockBytes::parse(m_Parser, m_cbFolderEntryID, _MaxEID);
+		m_cbFolderEntryID = blockT<DWORD>::parse(m_Parser);
+		m_lpFolderEntryID = blockBytes::parse(m_Parser, *m_cbFolderEntryID, _MaxEID);
 
-		m_cbMessageEntryID.parse<DWORD>(m_Parser);
-		m_lpMessageEntryID = blockBytes::parse(m_Parser, m_cbMessageEntryID, _MaxEID);
+		m_cbMessageEntryID = blockT<DWORD>::parse(m_Parser);
+		m_lpMessageEntryID = blockBytes::parse(m_Parser, *m_cbMessageEntryID, _MaxEID);
 
-		m_cbSearchFolderEntryID.parse<DWORD>(m_Parser);
-		m_lpSearchFolderEntryID = blockBytes::parse(m_Parser, m_cbSearchFolderEntryID, _MaxEID);
+		m_cbSearchFolderEntryID = blockT<DWORD>::parse(m_Parser);
+		m_lpSearchFolderEntryID = blockBytes::parse(m_Parser, *m_cbSearchFolderEntryID, _MaxEID);
 
-		m_cbMessageSearchKey.parse<DWORD>(m_Parser);
-		m_lpMessageSearchKey = blockBytes::parse(m_Parser, m_cbMessageSearchKey, _MaxEID);
+		m_cbMessageSearchKey = blockT<DWORD>::parse(m_Parser);
+		m_lpMessageSearchKey = blockBytes::parse(m_Parser, *m_cbMessageSearchKey, _MaxEID);
 
-		m_cchAnsiText.parse<DWORD>(m_Parser);
-		m_lpszAnsiText = blockStringA::parse(m_Parser, m_cchAnsiText);
+		m_cchAnsiText = blockT<DWORD>::parse(m_Parser);
+		m_lpszAnsiText = blockStringA::parse(m_Parser, *m_cchAnsiText);
 	}
 
-	void ReportTag::addEID(const std::wstring& label, const blockT<ULONG>& cb, std::shared_ptr<blockBytes>& eid)
+	void ReportTag::addEID(const std::wstring& label, const std::shared_ptr<blockT<ULONG>>& cb, std::shared_ptr<blockBytes>& eid)
 	{
-		if (cb)
+		if (*cb)
 		{
 			terminateBlock();
 			addHeader(label);
@@ -52,8 +52,8 @@ namespace smartview
 		addChild(m_Cookie);
 
 		terminateBlock();
-		auto szFlags = flags::InterpretFlags(flagReportTagVersion, m_Version);
-		addChild(m_Version, L"Version = 0x%1!08X! = %2!ws!", m_Version.getData(), szFlags.c_str());
+		auto szFlags = flags::InterpretFlags(flagReportTagVersion, *m_Version);
+		addChild(m_Version, L"Version = 0x%1!08X! = %2!ws!", m_Version->getData(), szFlags.c_str());
 
 		addEID(L"StoreEntryID = ", m_cbStoreEntryID, m_lpStoreEntryID);
 		addEID(L"FolderEntryID = ", m_cbFolderEntryID, m_lpFolderEntryID);
@@ -64,7 +64,7 @@ namespace smartview
 		if (m_cchAnsiText)
 		{
 			terminateBlock();
-			addChild(m_cchAnsiText, L"cchAnsiText = 0x%1!08X!\r\n", m_cchAnsiText.getData());
+			addChild(m_cchAnsiText, L"cchAnsiText = 0x%1!08X!\r\n", m_cchAnsiText->getData());
 			addChild(m_lpszAnsiText, L"AnsiText = %1!hs!", m_lpszAnsiText->c_str());
 		}
 	}
--- core/smartview/ReportTag.h
+++ core/smartview/ReportTag.h
@@ -12,21 +12,22 @@ namespace smartview
 	private:
 		void Parse() override;
 		void ParseBlocks() override;
-		void addEID(const std::wstring& label, const blockT<ULONG>& cb, std::shared_ptr<blockBytes>& eid);
+		void
+		addEID(const std::wstring& label, const std::shared_ptr<blockT<ULONG>>& cb, std::shared_ptr<blockBytes>& eid);
 
 		std::shared_ptr<blockBytes> m_Cookie = emptyBB(); // 8 characters + NULL terminator
-		blockT<DWORD> m_Version;
-		blockT<ULONG> m_cbStoreEntryID;
+		std::shared_ptr<blockT<DWORD>> m_Version = emptyT<DWORD>();
+		std::shared_ptr<blockT<ULONG>> m_cbStoreEntryID = emptyT<ULONG>();
 		std::shared_ptr<blockBytes> m_lpStoreEntryID = emptyBB();
-		blockT<ULONG> m_cbFolderEntryID;
+		std::shared_ptr<blockT<ULONG>> m_cbFolderEntryID = emptyT<ULONG>();
 		std::shared_ptr<blockBytes> m_lpFolderEntryID = emptyBB();
-		blockT<ULONG> m_cbMessageEntryID;
+		std::shared_ptr<blockT<ULONG>> m_cbMessageEntryID = emptyT<ULONG>();
 		std::shared_ptr<blockBytes> m_lpMessageEntryID = emptyBB();
-		blockT<ULONG> m_cbSearchFolderEntryID;
+		std::shared_ptr<blockT<ULONG>> m_cbSearchFolderEntryID = emptyT<ULONG>();
 		std::shared_ptr<blockBytes> m_lpSearchFolderEntryID = emptyBB();
-		blockT<ULONG> m_cbMessageSearchKey;
+		std::shared_ptr<blockT<ULONG>> m_cbMessageSearchKey = emptyT<ULONG>();
 		std::shared_ptr<blockBytes> m_lpMessageSearchKey = emptyBB();
-		blockT<ULONG> m_cchAnsiText;
+		std::shared_ptr<blockT<ULONG>> m_cchAnsiText = emptyT<ULONG>();
 		std::shared_ptr<blockStringA> m_lpszAnsiText = emptySA();
 	};
 } // namespace smartview
