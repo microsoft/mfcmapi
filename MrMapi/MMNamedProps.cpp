8a3d902ce950ba07013450e0db6c1546bea757c8
Brad Hughes <bradhugh@outlook.com>
2019-12-06 17:53:14 -0500

    Add sorting

--- MrMapi/MMNamedProps.cpp
+++ MrMapi/MMNamedProps.cpp
@@ -9,22 +9,54 @@
 #include <core/utility/output.h>
 #include <core/mapi/mapiFunctions.h>
 
-void PrintGuid(LPGUID lpGuid)
+void PrintGuid(const GUID& lpGuid)
 {
 	wprintf(
 		L"%.8lX-%.4X-%.4X-%.2X%.2X-%.2X%.2X%.2X%.2X%.2X%.2X",
-		lpGuid->Data1,
-		lpGuid->Data2,
-		lpGuid->Data3,
-		lpGuid->Data4[0],
-		lpGuid->Data4[1],
-		lpGuid->Data4[2],
-		lpGuid->Data4[3],
-		lpGuid->Data4[4],
-		lpGuid->Data4[5],
-		lpGuid->Data4[6],
-		lpGuid->Data4[7]);
+		lpGuid.Data1,
+		lpGuid.Data2,
+		lpGuid.Data3,
+		lpGuid.Data4[0],
+		lpGuid.Data4[1],
+		lpGuid.Data4[2],
+		lpGuid.Data4[3],
+		lpGuid.Data4[4],
+		lpGuid.Data4[5],
+		lpGuid.Data4[6],
+		lpGuid.Data4[7]);
 }
+
+class NamedPropEntry
+{
+public:
+	NamedPropEntry() : Kind(0), PropTag(0), PropSetGuid({0}), PropName(), PropId(0) {}
+
+	NamedPropEntry(ULONG propTag, LPGUID lpGuid, LPWSTR name)
+	{
+		this->Kind = MNID_STRING;
+		this->PropTag = propTag;
+		this->PropSetGuid = *lpGuid;
+		this->PropName = name;
+	}
+
+	NamedPropEntry(ULONG propTag, LPGUID lpGuid, ULONG id)
+	{
+		this->Kind = MNID_ID;
+		this->PropTag = propTag;
+		this->PropSetGuid = *lpGuid;
+		this->PropId = id;
+	}
+
+	/*NamedPropEntry(const ULONG propTag, const GUID& PropSetGuid, const std::wstring& name, const ULONG id)
+	{
+	}*/
+
+	ULONG PropTag;
+	GUID PropSetGuid;
+	ULONG Kind;
+	std::wstring PropName;
+	ULONG PropId;
+};
 
 void DoNamedProps(_In_opt_ LPMDB lpMDB)
 {
@@ -39,17 +71,55 @@ void DoNamedProps(_In_opt_ LPMDB lpMDB)
 	if (SUCCEEDED(hRes))
 	{
 		wprintf(L"\n");
+		std::vector<NamedPropEntry> props;
 		for (std::uint32_t i = 0; i < cPropNames; i++)
 		{
-			wprintf(L"[%08x] (", pProps->aulPropTag[i]);
-			PrintGuid(pNames[i]->lpguid);
 			switch (pNames[i]->ulKind)
 			{
 			case MNID_STRING:
-				wprintf(L":%ws)\n", pNames[i]->Kind.lpwstrName);
+				props.emplace_back(pProps->aulPropTag[i], pNames[i]->lpguid, pNames[i]->Kind.lpwstrName);
 				break;
 			case MNID_ID:
-				wprintf(L":0x%08x)\n", pNames[i]->Kind.lID);
+				props.emplace_back(pProps->aulPropTag[i], pNames[i]->lpguid, pNames[i]->Kind.lID);
+				break;
+			}
+		}
+
+		std::sort(props.begin(), props.end(), [](NamedPropEntry& i, NamedPropEntry& j) {
+			// If the Property set GUIDs don't match, sort by that
+			auto res = memcmp(&i.PropSetGuid, &j.PropSetGuid, sizeof(GUID));
+			if (res) return res < 0;
+
+			// If they are different kinds, use that next
+			if (i.Kind != j.Kind)
+			{
+				return i.Kind < j.Kind;
+			}
+
+			switch (i.Kind)
+			{
+			case MNID_ID:
+				return i.PropId < j.PropId;
+			case MNID_STRING:
+				return i.PropName < j.PropName;
+			}
+
+			// This shouldn't ever actually hit
+			return i.PropTag < j.PropTag;
+		});
+
+		for (int i = 0; i < props.size(); i++)
+		{
+			wprintf(L"[%08x] (", props[i].PropTag);
+			PrintGuid(props[i].PropSetGuid);
+			switch (props[i].Kind)
+			{
+			case MNID_ID:
+				wprintf(L":0x%08x)\n", props[i].PropId);
+				break;
+
+			case MNID_STRING:
+				wprintf(L":%ws)\n", props[i].PropName.c_str());
 				break;
 			}
 		}
