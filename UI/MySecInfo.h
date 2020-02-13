e868daff0088e2cdfca75540810af01b3283a4bf
Stephen Griffin <sgriffin@outlook.com>
2020-02-12 22:55:05 -0500

    fix com implementations

--- UI/MySecInfo.h
+++ UI/MySecInfo.h
@@ -14,22 +14,27 @@ namespace mapi::mapiui
 		STDMETHODIMP_(ULONG) AddRef() override;
 		STDMETHODIMP_(ULONG) Release() override;
 
-		STDMETHOD(GetObjectInformation)(PSI_OBJECT_INFO pObjectInfo) override;
-		STDMETHOD(GetSecurity)
-		(SECURITY_INFORMATION RequestedInformation, PSECURITY_DESCRIPTOR* ppSecurityDescriptor, BOOL fDefault) override;
-		STDMETHOD(SetSecurity)
-		(SECURITY_INFORMATION SecurityInformation, PSECURITY_DESCRIPTOR pSecurityDescriptor) override;
-		STDMETHOD(GetAccessRights)
-		(const GUID* pguidObjectType,
-		 DWORD dwFlags,
-		 PSI_ACCESS* ppAccess,
-		 ULONG* pcAccesses,
-		 ULONG* piDefaultAccess) override;
-		STDMETHOD(MapGeneric)(const GUID* pguidObjectType, UCHAR* pAceFlags, ACCESS_MASK* pMask) override;
-		STDMETHOD(GetInheritTypes)(PSI_INHERIT_TYPE* ppInheritTypes, ULONG* pcInheritTypes) override;
-		STDMETHOD(PropertySheetPageCallback)(HWND hwnd, UINT uMsg, SI_PAGE_TYPE uPage) override;
-		STDMETHOD_(BOOL, IsDaclCanonical)(PACL pDacl) override;
-		STDMETHOD(LookupSids)(ULONG cSids, PSID* rgpSids, LPDATAOBJECT* ppdo) override;
+		// *** ISecurityInformation methods ***
+		STDMETHODIMP GetObjectInformation(PSI_OBJECT_INFO pObjectInfo) override;
+		STDMETHODIMP GetSecurity(
+			SECURITY_INFORMATION RequestedInformation,
+			PSECURITY_DESCRIPTOR* ppSecurityDescriptor,
+			BOOL fDefault) override;
+		STDMETHODIMP
+		SetSecurity(SECURITY_INFORMATION SecurityInformation, PSECURITY_DESCRIPTOR pSecurityDescriptor) override;
+		STDMETHODIMP GetAccessRights(
+			const GUID* pguidObjectType,
+			DWORD dwFlags,
+			PSI_ACCESS* ppAccess,
+			ULONG* pcAccesses,
+			ULONG* piDefaultAccess) override;
+		STDMETHODIMP MapGeneric(const GUID* pguidObjectType, UCHAR* pAceFlags, ACCESS_MASK* pMask) override;
+		STDMETHODIMP GetInheritTypes(PSI_INHERIT_TYPE* ppInheritTypes, ULONG* pcInheritTypes) override;
+		STDMETHODIMP PropertySheetPageCallback(HWND hwnd, UINT uMsg, SI_PAGE_TYPE uPage) override;
+
+		// *** ISecurityInformation2 methods ***
+		STDMETHODIMP_(BOOL) IsDaclCanonical(PACL pDacl) override;
+		STDMETHODIMP LookupSids(ULONG cSids, PSID* rgpSids, LPDATAOBJECT* ppdo) override;
 
 	private:
 		LONG m_cRef;
