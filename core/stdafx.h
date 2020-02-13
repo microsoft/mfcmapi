b741adb105c37b171333b8fddec006930785c641
Stephen Griffin <sgriffin@microsoft.com>
2020-02-13 14:15:07 -0500

    suppress some big analysis buckets so we can focus on what's left

--- core/stdafx.h
+++ core/stdafx.h
@@ -1,5 +1,13 @@
 #pragma once
 
+// clang-format off
+#pragma warning(disable : 26426) // Warning C26426 Global initializer calls a non-constexpr (i.22)
+#pragma warning(disable : 26446) // Warning C26446 Prefer to use gsl::at() instead of unchecked subscript operator (bounds.4).
+#pragma warning(disable : 26481) // Warning C26481 Don't use pointer arithmetic. Use span instead (bounds.1).
+#pragma warning(disable : 26485) // Warning C26485 Expression '': No array to pointer decay (bounds.3).
+#pragma warning(disable : 26487) // Warning C26487 Don't return a pointer '' that may be invalid (lifetime.4).
+// clang-format on
+
 #define VC_EXTRALEAN // Exclude rarely-used stuff from Windows headers
 
 #define _WINSOCKAPI_ // stops windows.h including winsock.h
