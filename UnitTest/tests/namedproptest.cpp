5dc945d79b30a9ff80001d1b319eb6a705d179d0
Stephen Griffin <sgriffin@microsoft.com>
2020-03-11 15:55:54 -0400

    Add some more match tests

--- UnitTest/tests/namedproptest.cpp
+++ UnitTest/tests/namedproptest.cpp
@@ -17,19 +17,10 @@ namespace namedproptest
 		{
 			const auto sig1 = std::vector<BYTE>({1, 2, 3, 4});
 			const auto sig2 = std::vector<BYTE>({5, 6, 7, 8, 9});
-			const ULONG aulOneOffIDs[] = {dispidFormStorage,
-										  dispidPageDirStream,
-										  dispidFormPropStream,
-										  dispidScriptStream,
-										  dispidPropDefStream,
-										  dispidCustomFlag};
-			constexpr ULONG ulNumOneOffIDs = _countof(aulOneOffIDs);
 
 			const auto formStorageID = MAPINAMEID{const_cast<LPGUID>(&guid::PSETID_Common), MNID_ID, dispidFormStorage};
 			const auto pageDirStreamID =
 				MAPINAMEID{const_cast<LPGUID>(&guid::PSETID_Common), MNID_ID, dispidPageDirStream};
-			const auto formPropStreamID =
-				MAPINAMEID{const_cast<LPGUID>(&guid::PSETID_Common), MNID_ID, dispidFormPropStream};
 
 			const auto formStorage1 = cache::namedPropCacheEntry(&formStorageID, 0x1111, sig1);
 			const auto formStorage2 = cache::namedPropCacheEntry(&formStorageID, 0x1111, sig2);
@@ -44,16 +35,41 @@ namespace namedproptest
 			Assert::AreEqual(true, formStorage1.match(formStorage1, false, false, true));
 			Assert::AreEqual(true, formStorage1.match(formStorage1, false, false, false));
 
-			Assert::AreEqual(true, formStorage1.match(formStorage1, false, false, false));
-
 			// Should fail
 			Assert::AreEqual(false, formStorage1.match(formStorage2, true, true, true));
+
+			// Odd comparisons
+			Assert::AreEqual(
+				true, formStorage1.match(cache::namedPropCacheEntry(&formStorageID, 0x1111, sig1), true, true, true));
+			Assert::AreEqual(
+				true, formStorage1.match(cache::namedPropCacheEntry(&formStorageID, 0x1112, sig1), true, false, true));
+			Assert::AreEqual(
+				true,
+				formStorage1.match(cache::namedPropCacheEntry(&pageDirStreamID, 0x1111, sig1), true, true, false));
 
 			// Should all work
 			Assert::AreEqual(true, formStorage1.match(formStorage2, false, true, true));
 			Assert::AreEqual(true, formStorage1.match(formStorage2, false, false, true));
 			Assert::AreEqual(true, formStorage1.match(formStorage2, false, true, false));
 			Assert::AreEqual(true, formStorage1.match(formStorage2, false, false, false));
+
+			// Compare given a signature, MAPINAMEID
+			// _Check_return_ bool match(_In_ const std::vector<BYTE>& _sig, _In_ const MAPINAMEID& _mapiNameId) const;
+			Assert::AreEqual(true, formStorage1.match(sig1, formStorageID));
+			Assert::AreEqual(false, formStorage1.match(sig2, formStorageID));
+			Assert::AreEqual(false, formStorage1.match(sig1, pageDirStreamID));
+
+			// Compare given a signature and property ID (ulPropID)
+			// _Check_return_ bool match(_In_ const std::vector<BYTE>& _sig, ULONG _ulPropID) const;
+			Assert::AreEqual(true, formStorage1.match(sig1, 0x1111));
+			Assert::AreEqual(false, formStorage1.match(sig1, 0x1112));
+			Assert::AreEqual(false, formStorage1.match(sig2, 0x1111));
+
+			// Compare given a id, MAPINAMEID
+			// _Check_return_ bool match(ULONG _ulPropID, _In_ const MAPINAMEID& _mapiNameId) const noexcept;
+			Assert::AreEqual(true, formStorage1.match(0x1111, formStorageID));
+			Assert::AreEqual(false, formStorage1.match(0x1112, formStorageID));
+			Assert::AreEqual(false, formStorage1.match(0x1111, pageDirStreamID));
 		}
 	};
 } // namespace namedproptest
