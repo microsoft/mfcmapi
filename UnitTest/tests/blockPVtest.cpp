a5c6ce5c54f8dd4dac181b3f1c626c94765350aa
Stephen Griffin <sgriffin@outlook.com>
2020-05-05 20:30:52 -0400

    generalize pv test

--- UnitTest/tests/blockPVtest.cpp
+++ UnitTest/tests/blockPVtest.cpp
@@ -8,6 +8,26 @@ namespace blockPVtest
 	TEST_CLASS(blockPVtest)
 	{
 	private:
+		struct pvTestCase
+		{
+			std::shared_ptr<smartview::blockPV> block{};
+			ULONG ulPropTag;
+			std::shared_ptr<smartview::binaryParser> parser{};
+			size_t offset;
+			size_t size;
+			std::wstring text;
+			bool doNickname;
+			bool doRuleProcessing;
+		};
+
+		void testPV(pvTestCase & data)
+		{
+			data.block->parse(data.parser, data.ulPropTag, data.doNickname, data.doRuleProcessing);
+			Assert::AreEqual(data.offset, data.block->getOffset());
+			Assert::AreEqual(data.size, data.block->getSize());
+			Assert::AreEqual(data.text.c_str(), data.block->PropBlock()->c_str());
+		}
+
 		std::shared_ptr<smartview::binaryParser> makeParser(const std::wstring& str)
 		{
 			return std::make_shared<smartview::binaryParser>(strings::HexStringToBin(str));
@@ -21,15 +41,16 @@ namespace blockPVtest
 
 		TEST_METHOD(Test_PT_BINARY)
 		{
-			auto block1 = std::make_shared<smartview::SBinaryBlock>();
-			auto bin1 =
-				makeParser(L"250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D");
-			block1->parse(bin1, PR_SENDER_SEARCH_KEY);
-			Assert::AreEqual(size_t{0}, block1->PropBlock()->getOffset());
-			Assert::AreEqual(size_t{0x29}, block1->PropBlock()->getSize());
-			Assert::AreEqual(
+			auto t1 = pvTestCase{
+				std::make_shared<smartview::SBinaryBlock>(),
+				PR_SENDER_SEARCH_KEY,
+				makeParser(L"250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D"),
+				0,
+				0x29,
 				L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
-				block1->PropBlock()->c_str());
+				false,
+				true};
+			testPV(t1);
 		}
 	};
 } // namespace blockPVtest
