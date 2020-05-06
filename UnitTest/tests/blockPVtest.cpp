ec62b72ea748856bd530cead9820c8b6c9bc7840
Stephen Griffin <sgriffin@outlook.com>
2020-05-06 19:48:14 -0400

    batch cases

--- UnitTest/tests/blockPVtest.cpp
+++ UnitTest/tests/blockPVtest.cpp
@@ -29,6 +29,14 @@ namespace blockPVtest
 			Assert::AreEqual(data.text.c_str(), data.block->PropBlock()->c_str());
 		}
 
+		void testPV(std::vector<pvTestCase> & testCases)
+		{
+			for (auto test : testCases)
+			{
+				testPV(test);
+			}
+		}
+
 		std::shared_ptr<smartview::binaryParser> makeParser(const std::wstring& str)
 		{
 			return std::make_shared<smartview::binaryParser>(strings::HexStringToBin(str));
@@ -42,7 +50,7 @@ namespace blockPVtest
 
 		TEST_METHOD(Test_PT_BINARY)
 		{
-			auto t1 =
+			auto testData = std::vector<pvTestCase>{
 				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
 						   PR_SENDER_SEARCH_KEY,
 						   L"250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
@@ -50,9 +58,7 @@ namespace blockPVtest
 						   0x29,
 						   L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   false,
-						   true};
-			testPV(t1);
-			auto t2 =
+						   true},
 				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
 						   PR_SENDER_SEARCH_KEY,
 						   L"25004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
@@ -60,18 +66,17 @@ namespace blockPVtest
 						   0x27,
 						   L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   false,
-						   false};
-			testPV(t2);
-			auto t3 =
+						   false},
 				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
 						   PR_SENDER_SEARCH_KEY,
-						   L"0000000000000000250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
+						   L"0000000000000000250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D424552"
+						   L"2E434F4D",
 						   0,
 						   0x31,
 						   L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   true,
-						   false};
-			testPV(t3);
+						   false}};
+			testPV(testData);
 		}
 	};
 } // namespace blockPVtest
