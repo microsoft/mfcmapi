fe259c940f254541b447c77a8642f87a2fe7c819
Stephen Griffin <sgriffin@outlook.com>
2020-05-06 20:06:53 -0400

    get block for testing on demand

--- UnitTest/tests/blockPVtest.cpp
+++ UnitTest/tests/blockPVtest.cpp
@@ -10,7 +10,6 @@ namespace blockPVtest
 	private:
 		struct pvTestCase
 		{
-			std::shared_ptr<smartview::blockPV> block{};
 			ULONG ulPropTag;
 			std::wstring source{};
 			size_t offset;
@@ -22,11 +21,13 @@ namespace blockPVtest
 
 		void testPV(pvTestCase & data)
 		{
+			auto block = smartview::getPVParser(PROP_TYPE(data.ulPropTag));
 			auto parser = makeParser(data.source);
-			data.block->parse(parser, data.ulPropTag, data.doNickname, data.doRuleProcessing);
-			Assert::AreEqual(data.offset, data.block->getOffset());
-			Assert::AreEqual(data.size, data.block->getSize());
-			Assert::AreEqual(data.text.c_str(), data.block->PropBlock()->c_str());
+
+			block->parse(parser, data.ulPropTag, data.doNickname, data.doRuleProcessing);
+			Assert::AreEqual(data.offset, block->getOffset());
+			Assert::AreEqual(data.size, block->getSize());
+			Assert::AreEqual(data.text.c_str(), block->PropBlock()->c_str());
 		}
 
 		void testPV(std::vector<pvTestCase> & testCases)
@@ -51,24 +52,21 @@ namespace blockPVtest
 		TEST_METHOD(Test_PT_BINARY)
 		{
 			auto testData = std::vector<pvTestCase>{
-				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
-						   PR_SENDER_SEARCH_KEY,
+				pvTestCase{PR_SENDER_SEARCH_KEY,
 						   L"250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   0,
 						   0x29,
 						   L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   false,
 						   true},
-				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
-						   PR_SENDER_SEARCH_KEY,
+				pvTestCase{PR_SENDER_SEARCH_KEY,
 						   L"25004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   0,
 						   0x27,
 						   L"cb: 37 lpb: 4449534E45595641434154494F4E434C5542404D41494C2E4456434D454D4245522E434F4D",
 						   false,
 						   false},
-				pvTestCase{std::make_shared<smartview::SBinaryBlock>(),
-						   PR_SENDER_SEARCH_KEY,
+				pvTestCase{PR_SENDER_SEARCH_KEY,
 						   L"0000000000000000250000004449534E45595641434154494F4E434C5542404D41494C2E4456434D454D424552"
 						   L"2E434F4D",
 						   0,
