177cb82c88fcc4aa6b8feecbd69268ccca9be926
Stephen Griffin <sgriffin@outlook.com>
2020-05-15 22:18:42 -0400

    Fix unicode case

--- UnitTest/tests/proptagTest.cpp
+++ UnitTest/tests/proptagTest.cpp
@@ -16,22 +16,22 @@ namespace proptagTest
 		{
 			unittest::AreEqualEx(
 				L"0x0037001E (PT_STRING8): PR_SUBJECT: (PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject)",
-				proptags::TagToString(PR_SUBJECT, nullptr, false, true));
+				proptags::TagToString(PR_SUBJECT_A, nullptr, false, true));
 			unittest::AreEqualEx(
 				L"Tag: 0x0037001E\r\n"
 				L"Type: PT_STRING8\r\n"
 				L"Property Name: PR_SUBJECT\r\n"
 				L"Other Names: PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject\r\n"
 				L"DASL: http://schemas.microsoft.com/mapi/proptag/0x0037001E",
-				proptags::TagToString(PR_SUBJECT, nullptr, false, false));
+				proptags::TagToString(PR_SUBJECT_A, nullptr, false, false));
 		}
 
 		TEST_METHOD(Test_PropTagToPropName)
 		{
-			unittest::AreEqualEx(L"PR_SUBJECT", proptags::PropTagToPropName(PR_SUBJECT, false).bestGuess);
+			unittest::AreEqualEx(L"PR_SUBJECT", proptags::PropTagToPropName(PR_SUBJECT_A, false).bestGuess);
 			unittest::AreEqualEx(
 				L"PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject",
-				proptags::PropTagToPropName(PR_SUBJECT, false).otherMatches);
+				proptags::PropTagToPropName(PR_SUBJECT_A, false).otherMatches);
 
 			unittest::AreEqualEx(L"PR_RW_RULES_STREAM", proptags::PropTagToPropName(0x68020102, false).bestGuess);
 			unittest::AreEqualEx(
