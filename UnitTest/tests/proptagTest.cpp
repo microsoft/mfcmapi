46f6c861eefa3cfd648a138ca644d4c40253d479
Stephen Griffin <sgriffin@outlook.com>
2020-05-15 21:32:46 -0400

    more tests

--- UnitTest/tests/proptagTest.cpp
+++ UnitTest/tests/proptagTest.cpp
@@ -15,16 +15,17 @@ namespace proptagTest
 		TEST_METHOD(Test_TagToString)
 		{
 			unittest::AreEqualEx(
-				std::wstring(
-					L"0x0037001E (PT_STRING8): PR_SUBJECT: (PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject)"),
+				L"0x0037001E (PT_STRING8): PR_SUBJECT: (PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject)",
 				proptags::TagToString(PR_SUBJECT, nullptr, false, true));
 			unittest::AreEqualEx(
-				std::wstring(L"Tag: 0x0037001E\r\n"
-							 L"Type: PT_STRING8\r\n"
-							 L"Property Name: PR_SUBJECT\r\n"
-							 L"Other Names: PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject\r\n"
-							 L"DASL: http://schemas.microsoft.com/mapi/proptag/0x0037001E"),
+				L"Tag: 0x0037001E\r\n"
+				L"Type: PT_STRING8\r\n"
+				L"Property Name: PR_SUBJECT\r\n"
+				L"Other Names: PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject\r\n"
+				L"DASL: http://schemas.microsoft.com/mapi/proptag/0x0037001E",
 				proptags::TagToString(PR_SUBJECT, nullptr, false, false));
+			unittest::AreEqualEx(L"PR_SUBJECT", proptags::PropTagToPropName(PR_SUBJECT, false).bestGuess);
+			unittest::AreEqualEx(L"PR_SUBJECT_A, ptagSubject, PR_SUBJECT_W, PidTagSubject", proptags::PropTagToPropName(PR_SUBJECT, false).otherMatches);
 		}
 	};
 } // namespace proptagTest
