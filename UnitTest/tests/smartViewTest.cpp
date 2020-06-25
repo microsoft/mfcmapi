2bb11aa4186f1377164f911d01e5996cff38216e
Stephen Griffin <sgriffin@outlook.com>
2020-06-24 21:11:39 -0400

    simplify test

--- UnitTest/tests/smartViewTest.cpp
+++ UnitTest/tests/smartViewTest.cpp
@@ -26,8 +26,9 @@ namespace SmartViewTest
 			std::wstring expected;
 		};
 
-		void test(std::vector<SmartViewTestData> testData) const
+		void test(const std::initializer_list<SmartViewTestResource>& resources) const
 		{
+			auto testData = loadTestData(resources);
 			for (auto& data : testData)
 			{
 				auto actual = smartview::InterpretBinaryAsString(
@@ -81,37 +82,37 @@ namespace SmartViewTest
 
 		TEST_METHOD(Test_STADDITIONALRENENTRYIDSEX)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::ADDITIONALRENENTRYIDSEX, IDR_SV1AEI1IN, IDR_SV1AEI1OUT},
 				SmartViewTestResource{parserType::ADDITIONALRENENTRYIDSEX, IDR_SV1AEI2IN, IDR_SV1AEI2OUT},
 				SmartViewTestResource{parserType::ADDITIONALRENENTRYIDSEX, IDR_SV1AEI3IN, IDR_SV1AEI3OUT},
 				SmartViewTestResource{parserType::ADDITIONALRENENTRYIDSEX, IDR_SV1AEI4IN, IDR_SV1AEI4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STAPPOINTMENTRECURRENCEPATTERN)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::APPOINTMENTRECURRENCEPATTERN, IDR_SV2ARP1IN, IDR_SV2ARP1OUT},
 				SmartViewTestResource{parserType::APPOINTMENTRECURRENCEPATTERN, IDR_SV2ARP2IN, IDR_SV2ARP2OUT},
 				SmartViewTestResource{parserType::APPOINTMENTRECURRENCEPATTERN, IDR_SV2ARP3IN, IDR_SV2ARP3OUT},
 				SmartViewTestResource{parserType::APPOINTMENTRECURRENCEPATTERN, IDR_SV2ARP4IN, IDR_SV2ARP4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STCONVERSATIONINDEX)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::CONVERSATIONINDEX, IDR_SV3CI1IN, IDR_SV3CI1OUT},
 				SmartViewTestResource{parserType::CONVERSATIONINDEX, IDR_SV3CI2IN, IDR_SV3CI2OUT},
 				SmartViewTestResource{parserType::CONVERSATIONINDEX, IDR_SV3CI3IN, IDR_SV3CI3OUT},
 				SmartViewTestResource{parserType::CONVERSATIONINDEX, IDR_SV3CI4IN, IDR_SV3CI4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STENTRYID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID1IN, IDR_SV4EID1OUT},
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID2IN, IDR_SV4EID2OUT},
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID3IN, IDR_SV4EID3OUT},
@@ -152,66 +153,66 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID38IN, IDR_SV4EID38OUT},
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID39IN, IDR_SV4EID39OUT},
 				SmartViewTestResource{parserType::ENTRYID, IDR_SV4EID40IN, IDR_SV4EID40OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STENTRYLIST)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::ENTRYLIST, IDR_SV5EL1IN, IDR_SV5EL1OUT},
 				SmartViewTestResource{parserType::ENTRYLIST, IDR_SV5EL2IN, IDR_SV5EL2OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STEXTENDEDFOLDERFLAGS)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::EXTENDEDFOLDERFLAGS, IDR_SV6EFF1IN, IDR_SV6EFF1OUT},
 				SmartViewTestResource{parserType::EXTENDEDFOLDERFLAGS, IDR_SV6EFF2IN, IDR_SV6EFF2OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STEXTENDEDRULECONDITION)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::EXTENDEDRULECONDITION, IDR_SV7EXRULE1IN, IDR_SV7EXRULE1OUT},
 				SmartViewTestResource{parserType::EXTENDEDRULECONDITION, IDR_SV7EXRULE2IN, IDR_SV7EXRULE2OUT},
 				SmartViewTestResource{parserType::EXTENDEDRULECONDITION, IDR_SV7EXRULE3IN, IDR_SV7EXRULE3OUT},
 				SmartViewTestResource{parserType::EXTENDEDRULECONDITION, IDR_SV7EXRULE4IN, IDR_SV7EXRULE4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STFLATENTRYLIST)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::FLATENTRYLIST, IDR_SV8FE1IN, IDR_SV8FE1OUT},
 				SmartViewTestResource{parserType::FLATENTRYLIST, IDR_SV8FE2IN, IDR_SV8FE2OUT},
 				SmartViewTestResource{parserType::FLATENTRYLIST, IDR_SV8FE3IN, IDR_SV8FE3OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STFOLDERUSERFIELDS)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::FOLDERUSERFIELDS, IDR_SV9FUF1IN, IDR_SV9FUF1OUT},
 				SmartViewTestResource{parserType::FOLDERUSERFIELDS, IDR_SV9FUF2IN, IDR_SV9FUF2OUT},
 				SmartViewTestResource{parserType::FOLDERUSERFIELDS, IDR_SV9FUF3IN, IDR_SV9FUF3OUT},
 				SmartViewTestResource{parserType::FOLDERUSERFIELDS, IDR_SV9FUF4IN, IDR_SV9FUF4OUT},
 				SmartViewTestResource{parserType::FOLDERUSERFIELDS, IDR_SV9FUF5IN, IDR_SV9FUF5OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STGLOBALOBJECTID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::GLOBALOBJECTID, IDR_SV10GOID1IN, IDR_SV10GOID1OUT},
 				SmartViewTestResource{parserType::GLOBALOBJECTID, IDR_SV10GOID2IN, IDR_SV10GOID2OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STPROPERTY)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP1IN, IDR_SV11PROP1OUT},
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP2IN, IDR_SV11PROP2OUT},
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP3IN, IDR_SV11PROP3OUT},
@@ -220,12 +221,12 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP6IN, IDR_SV11PROP6OUT},
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP7IN, IDR_SV11PROP7OUT},
 				SmartViewTestResource{parserType::PROPERTIES, IDR_SV11PROP8IN, IDR_SV11PROP8OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STPROPERTYDEFINITIONSTREAM)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF1IN, IDR_SV12PROPDEF1OUT},
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF2IN, IDR_SV12PROPDEF2OUT},
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF3IN, IDR_SV12PROPDEF3OUT},
@@ -234,56 +235,56 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF6IN, IDR_SV12PROPDEF6OUT},
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF7IN, IDR_SV12PROPDEF7OUT},
 				SmartViewTestResource{parserType::PROPERTYDEFINITIONSTREAM, IDR_SV12PROPDEF8IN, IDR_SV12PROPDEF8OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STRECIPIENTROWSTREAM)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::RECIPIENTROWSTREAM, IDR_SV13RECIPROW1IN, IDR_SV13RECIPROW1OUT},
 				SmartViewTestResource{parserType::RECIPIENTROWSTREAM, IDR_SV13RECIPROW2IN, IDR_SV13RECIPROW2OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STRECURRENCEPATTERN)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::RECURRENCEPATTERN, IDR_SV14ARP1IN, IDR_SV14ARP1OUT},
 				SmartViewTestResource{parserType::RECURRENCEPATTERN, IDR_SV14ARP2IN, IDR_SV14ARP2OUT},
 				SmartViewTestResource{parserType::RECURRENCEPATTERN, IDR_SV14ARP3IN, IDR_SV14ARP3OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STREPORTTAG)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::REPORTTAG, IDR_SV15REPORTTAG1IN, IDR_SV15REPORTTAG1OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STRESTRICTION)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::RESTRICTION, IDR_SV16RES1IN, IDR_SV16RES1OUT},
 				SmartViewTestResource{parserType::RESTRICTION, IDR_SV16RES2IN, IDR_SV16RES2OUT},
 				SmartViewTestResource{parserType::RESTRICTION, IDR_SV16RES3IN, IDR_SV16RES3OUT},
 				SmartViewTestResource{parserType::RESTRICTION, IDR_SV16RES4IN, IDR_SV16RES4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STRULECONDITION)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::RULECONDITION, IDR_SV17RULECON1IN, IDR_SV17RULECON1OUT},
 				SmartViewTestResource{parserType::RULECONDITION, IDR_SV17RULECON2IN, IDR_SV17RULECON2OUT},
 				SmartViewTestResource{parserType::RULECONDITION, IDR_SV17RULECON3IN, IDR_SV17RULECON3OUT},
 				SmartViewTestResource{parserType::RULECONDITION, IDR_SV17RULECON4IN, IDR_SV17RULECON4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STSEARCHFOLDERDEFINITION)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF1IN, IDR_SV18SF1OUT},
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF2IN, IDR_SV18SF2OUT},
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF3IN, IDR_SV18SF3OUT},
@@ -291,56 +292,56 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF5IN, IDR_SV18SF5OUT},
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF6IN, IDR_SV18SF6OUT},
 				SmartViewTestResource{parserType::SEARCHFOLDERDEFINITION, IDR_SV18SF7IN, IDR_SV18SF7OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STSECURITYDESCRIPTOR)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::SECURITYDESCRIPTOR, IDR_SV19SD1IN, IDR_SV19SD1OUT},
 				SmartViewTestResource{parserType::SECURITYDESCRIPTOR, IDR_SV19SD2IN, IDR_SV19SD2OUT},
 				SmartViewTestResource{parserType::SECURITYDESCRIPTOR, IDR_SV19SD3IN, IDR_SV19SD3OUT},
 				SmartViewTestResource{parserType::SECURITYDESCRIPTOR, IDR_SV19SD4IN, IDR_SV19SD4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STSID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::SID, IDR_SV20SID1IN, IDR_SV20SID1OUT},
 				SmartViewTestResource{parserType::SID, IDR_SV20SID2IN, IDR_SV20SID2OUT},
 				SmartViewTestResource{parserType::SID, IDR_SV20SID3IN, IDR_SV20SID3OUT},
 				SmartViewTestResource{parserType::SID, IDR_SV20SID4IN, IDR_SV20SID4OUT},
 				SmartViewTestResource{parserType::SID, IDR_SV20SID5IN, IDR_SV20SID5OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STTASKASSIGNERS)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::TASKASSIGNERS, IDR_SV21TA1IN, IDR_SV21TA1OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STTIMEZONE)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::TIMEZONE, IDR_SV22TZ1IN, IDR_SV22TZ1OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STTIMEZONEDEFINITION)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::TIMEZONEDEFINITION, IDR_SV23TZD1IN, IDR_SV23TZD1OUT},
 				SmartViewTestResource{parserType::TIMEZONEDEFINITION, IDR_SV23TZD2IN, IDR_SV23TZD2OUT},
 				SmartViewTestResource{parserType::TIMEZONEDEFINITION, IDR_SV23TZD3IN, IDR_SV23TZD3OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STWEBVIEWPERSISTSTREAM)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW1IN, IDR_SV24WEBVIEW1OUT},
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW2IN, IDR_SV24WEBVIEW2OUT},
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW3IN, IDR_SV24WEBVIEW3OUT},
@@ -348,35 +349,35 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW5IN, IDR_SV24WEBVIEW5OUT},
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW6IN, IDR_SV24WEBVIEW6OUT},
 				SmartViewTestResource{parserType::WEBVIEWPERSISTSTREAM, IDR_SV24WEBVIEW7IN, IDR_SV24WEBVIEW7OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STNICKNAMECACHE)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::NICKNAMECACHE, IDR_SV25NICKNAME2IN, IDR_SV25NICKNAME2OUT},
 				SmartViewTestResource{parserType::NICKNAMECACHE, IDR_SV25NICKNAME3IN, IDR_SV25NICKNAME3OUT},
 				SmartViewTestResource{parserType::NICKNAMECACHE, IDR_SV25NICKNAME4IN, IDR_SV25NICKNAME4OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STENCODEENTRYID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::ENCODEENTRYID, IDR_SV26EIDENCODE1IN, IDR_SV26EIDENCODE1OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STDECODEENTRYID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::DECODEENTRYID, IDR_SV27EIDDECODE1IN, IDR_SV27EIDDECODE1OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STVERBSTREAM)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM1IN, IDR_SV28VERBSTREAM1OUT},
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM2IN, IDR_SV28VERBSTREAM2OUT},
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM3IN, IDR_SV28VERBSTREAM3OUT},
@@ -384,45 +385,45 @@ namespace SmartViewTest
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM5IN, IDR_SV28VERBSTREAM5OUT},
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM6IN, IDR_SV28VERBSTREAM6OUT},
 				SmartViewTestResource{parserType::VERBSTREAM, IDR_SV28VERBSTREAM7IN, IDR_SV28VERBSTREAM7OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STTOMBSTONE)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::TOMBSTONE, IDR_SV29TOMBSTONE1IN, IDR_SV29TOMBSTONE1OUT},
 				SmartViewTestResource{parserType::TOMBSTONE, IDR_SV29TOMBSTONE2IN, IDR_SV29TOMBSTONE2OUT},
 				SmartViewTestResource{parserType::TOMBSTONE, IDR_SV29TOMBSTONE3IN, IDR_SV29TOMBSTONE3OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STPCL)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::PCL, IDR_SV30PCL1IN, IDR_SV30PCL1OUT},
 				SmartViewTestResource{parserType::PCL, IDR_SV30PCL2IN, IDR_SV30PCL2OUT},
 				SmartViewTestResource{parserType::PCL, IDR_SV30PCL3IN, IDR_SV30PCL3OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STFBSECURITYDESCRIPTOR)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{
 					parserType::FBSECURITYDESCRIPTOR, IDR_SV31FREEBUSYSID1IN, IDR_SV31FREEBUSYSID1OUT},
 				SmartViewTestResource{
 					parserType::FBSECURITYDESCRIPTOR, IDR_SV31FREEBUSYSID2IN, IDR_SV31FREEBUSYSID2OUT},
-			}));
+			});
 		}
 
 		TEST_METHOD(Test_STXID)
 		{
-			test(loadTestData({
+			test({
 				SmartViewTestResource{parserType::XID, IDR_SV32XID1IN, IDR_SV32XID1OUT},
 				SmartViewTestResource{parserType::XID, IDR_SV32XID2IN, IDR_SV32XID2OUT},
 				SmartViewTestResource{parserType::XID, IDR_SV32XID3IN, IDR_SV32XID3OUT},
 				SmartViewTestResource{parserType::XID, IDR_SV32XID4IN, IDR_SV32XID4OUT},
-			}));
+			});
 		}
 	};
 } // namespace SmartViewTest
