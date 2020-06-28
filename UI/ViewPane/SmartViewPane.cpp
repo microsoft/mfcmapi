689c716a01c4413b08ff00f9ac2c301d12d74175
Stephen Griffin <sgriffin@outlook.com>
2020-06-28 16:03:52 -0400

    dont recycle parser

--- UI/ViewPane/SmartViewPane.cpp
+++ UI/ViewPane/SmartViewPane.cpp
@@ -182,11 +182,11 @@ namespace viewpane
 		const auto iStructType = static_cast<parserType>(GetDropDownSelectionValue());
 		auto szSmartViewArray = std::vector<std::wstring>{};
 		treeData = smartview::block::create();
-		auto svp = smartview::GetSmartViewParser(iStructType, nullptr);
 		auto source = 0;
 		for (auto& bin : m_bins)
 		{
 			auto parsedData = std::wstring{};
+			auto svp = smartview::GetSmartViewParser(iStructType, nullptr);
 			if (svp)
 			{
 				svp->init(bin.size(), bin.data());
