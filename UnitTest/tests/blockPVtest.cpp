b2db58591626c6f221e53b0ce723b9b6d32b3b1e
Stephen Griffin <sgriffin@outlook.com>
2020-07-06 20:19:09 -0400

    update tests

--- UnitTest/tests/blockPVtest.cpp
+++ UnitTest/tests/blockPVtest.cpp
@@ -44,13 +44,12 @@ namespace blockPVtest
 
 		TEST_METHOD(Test_PT_BINARY)
 		{
-			auto propblock = std::wstring(
-				L"PropString = cb: 20 lpb: 516B013F8BAD5B4D9A74CAB5B37B588400000006\r\n"
-				L"AltPropString = Qk.?.≠[M.t µ≥{X.....\r\n"
-				L"Smart View\r\n"
-				L"\tXID\r\n"
-				L"\t\tNamespaceGuid = {3F016B51-AD8B-4D5B-9A74-CAB5B37B5884}\r\n"
-				L"\t\tLocalId = cb: 4 lpb: 00000006");
+			auto propblock = std::wstring(L"PropString = cb: 20 lpb: 516B013F8BAD5B4D9A74CAB5B37B588400000006\r\n"
+										  L"AltPropString = Qk.?.≠[M.t µ≥{X.....\r\n"
+										  L"Smart View\r\n"
+										  L"\tXID\r\n"
+										  L"\t\tNamespaceGuid = {3F016B51-AD8B-4D5B-9A74-CAB5B37B5884}\r\n"
+										  L"\t\tLocalId = cb: 4 lpb: 00000006");
 			testPV(
 				L"bin-f-f",
 				PR_CHANGE_KEY,
@@ -85,9 +84,8 @@ namespace blockPVtest
 
 		TEST_METHOD(Test_PT_UNICODE)
 		{
-			auto propblock = std::wstring(
-				L"PropString = test string\r\n"
-				L"AltPropString = cb: 22 lpb: 7400650073007400200073007400720069006E006700");
+			auto propblock = std::wstring(L"PropString = test string\r\n"
+										  L"AltPropString = cb: 22 lpb: 7400650073007400200073007400720069006E006700");
 			auto smartview = std::wstring(L"");
 			testPV(
 				L"uni-f-f",
@@ -123,116 +121,35 @@ namespace blockPVtest
 
 		TEST_METHOD(Test_PT_LONG)
 		{
-			auto propblock = std::wstring(
-				L"PropString = 9\r\n"
-				L"AltPropString = 0x9\r\n"
-				L"Smart View\r\n"
-				L"\tFlags: MAPI_PROFSECT");
+			auto propblock = std::wstring(L"PropString = 9\r\n"
+										  L"AltPropString = 0x9\r\n"
+										  L"Smart View\r\n"
+										  L"\tFlags: MAPI_PROFSECT");
 			auto toNum = std::wstring(L"MAPI_PROFSECT");
-			testPV(
-				L"long-f-f",
-				PR_OBJECT_TYPE,
-				L"09000000",
-				false,
-				false,
-				propblock,
-				toNum,
-				0,
-				0x4);
-			testPV(
-				L"long-f-t",
-				PR_OBJECT_TYPE,
-				L"09000000",
-				false,
-				true,
-				propblock,
-				toNum,
-				0,
-				0x4);
-			testPV(
-				L"long-t-f",
-				PR_OBJECT_TYPE,
-				L"0900000000000000",
-				true,
-				false,
-				propblock,
-				toNum,
-				0,
-				0x8);
+			testPV(L"long-f-f", PR_OBJECT_TYPE, L"09000000", false, false, propblock, toNum, 0, 0x4);
+			testPV(L"long-f-t", PR_OBJECT_TYPE, L"09000000", false, true, propblock, toNum, 0, 0x4);
+			testPV(L"long-t-f", PR_OBJECT_TYPE, L"0900000000000000", true, false, propblock, toNum, 0, 0x8);
 		}
 
 		TEST_METHOD(Test_PT_SYSTIME)
 		{
-			auto propblock = std::wstring(
-				L"PropString = 03:59:00.000 AM 5/10/2020\r\n"
-				L"AltPropString = Low: 0x5606DA00 High: 0x01D6267F");
+			auto propblock = std::wstring(L"PropString = 03:59:00.000 AM 5/10/2020\r\n"
+										  L"AltPropString = Low: 0x5606DA00 High: 0x01D6267F");
 			auto toNum = std::wstring(L"");
 			testPV(
-				L"systime-f-f",
-				PR_MESSAGE_DELIVERY_TIME,
-				L"00DA06567F26D601",
-				false,
-				false,
-				propblock,
-				toNum,
-				0,
-				0x8);
+				L"systime-f-f", PR_MESSAGE_DELIVERY_TIME, L"00DA06567F26D601", false, false, propblock, toNum, 0, 0x8);
 			testPV(
-				L"systime-f-t",
-				PR_MESSAGE_DELIVERY_TIME,
-				L"00DA06567F26D601",
-				false,
-				true,
-				propblock,
-				toNum,
-				0,
-				0x8);
+				L"systime-f-t", PR_MESSAGE_DELIVERY_TIME, L"00DA06567F26D601", false, true, propblock, toNum, 0, 0x8);
 			testPV(
-				L"systime-t-f",
-				PR_MESSAGE_DELIVERY_TIME,
-				L"00DA06567F26D601",
-				true,
-				false,
-				propblock,
-				toNum,
-				0,
-				0x8);
+				L"systime-t-f", PR_MESSAGE_DELIVERY_TIME, L"00DA06567F26D601", true, false, propblock, toNum, 0, 0x8);
 		}
 
 		TEST_METHOD(Test_PT_BOOLEAN)
 		{
-			testPV(
-				L"bool-f-f-1",
-				PR_MESSAGE_TO_ME,
-				L"0100",
-				false,
-				false,
-				L"PropString = True",
-				L"",
-				0,
-				0x2);
-			testPV(
-				L"bool-f-f" - 2,
-				PR_MESSAGE_TO_ME,
-				L"0000",
-				false,
-				false,
-				L"PropString = False",
-				L"",
-				0,
-				0x2);
-			testPV(
-				L"bool-f-t", PR_MESSAGE_TO_ME, L"01", false, true, L"PropString = True", L"", 0, 0x1);
-			testPV(
-				L"bool-t-f",
-				PR_MESSAGE_TO_ME,
-				L"0100000000000000",
-				true,
-				false,
-				L"PropString = True",
-				L"",
-				0,
-				0x8);
+			testPV(L"bool-f-f-1", PR_MESSAGE_TO_ME, L"0100", false, false, L"PropString = True", L"", 0, 0x2);
+			testPV(L"bool-f-f" - 2, PR_MESSAGE_TO_ME, L"0000", false, false, L"PropString = False", L"", 0, 0x2);
+			testPV(L"bool-f-t", PR_MESSAGE_TO_ME, L"01", false, true, L"PropString = True", L"", 0, 0x1);
+			testPV(L"bool-t-f", PR_MESSAGE_TO_ME, L"0100000000000000", true, false, L"PropString = True", L"", 0, 0x8);
 		}
 	};
 } // namespace blockPVtest
