88c3055fd8459c543ed12292b42dacea3915f795
Stephen Griffin <sgriffin@outlook.com>
2020-12-20 00:04:56 -0500

    hide more

--- core/sortlistdata/propListData.h
+++ core/sortlistdata/propListData.h
@@ -11,7 +11,10 @@ namespace sortlistdata
 		static void init(sortListData* data, _In_ ULONG ulPropTag);
 
 		propListData(_In_ ULONG ulPropTag) noexcept;
+		_Check_return_ ULONG getPropTag() { return m_ulPropTag; }
+		void setPropTag(ULONG ulPropTag) { m_ulPropTag = ulPropTag; }
 
+	private:
 		ULONG m_ulPropTag{};
 	};
 } // namespace sortlistdata
