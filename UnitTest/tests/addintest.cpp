e0c0fa2ee0616b671eb914b2a19d1e0f272a4e3e
Stephen Griffin <sgriffin@outlook.com>
2021-03-29 22:28:06 -0400

    add some more tests

--- UnitTest/tests/addintest.cpp
+++ UnitTest/tests/addintest.cpp
@@ -8,8 +8,8 @@ namespace addin
 	// imports for testing
 	int _cdecl compareTypes(_In_ const void* a1, _In_ const void* a2) noexcept;
 	int _cdecl compareTags(_In_ const void* a1, _In_ const void* a2) noexcept;
-	//int _cdecl compareNameID(_In_ const void* a1, _In_ const void* a2) noexcept;
-	//int _cdecl compareSmartViewParser(_In_ const void* a1, _In_ const void* a2) noexcept;
+	int _cdecl compareNameID(_In_ const void* a1, _In_ const void* a2) noexcept;
+	int _cdecl compareSmartViewParser(_In_ const void* a1, _In_ const void* a2) noexcept;
 } // namespace addin
 
 namespace addintest
@@ -22,6 +22,18 @@ namespace addintest
 	int _cdecl testCompareTags(_In_ const NAME_ARRAY_ENTRY_V2& a1, _In_ const NAME_ARRAY_ENTRY_V2& a2) noexcept
 	{
 		return addin::compareTags(reinterpret_cast<LPCVOID>(&a1), reinterpret_cast<LPCVOID>(&a2));
+	}
+
+	int _cdecl testCompareNameID(_In_ const NAMEID_ARRAY_ENTRY& a1, _In_ const NAMEID_ARRAY_ENTRY& a2) noexcept
+	{
+		return addin::compareNameID(reinterpret_cast<LPCVOID>(&a1), reinterpret_cast<LPCVOID>(&a2));
+	}
+
+	int _cdecl testCompareSmartViewParser(
+		_In_ const SMARTVIEW_PARSER_ARRAY_ENTRY& a1,
+		_In_ const SMARTVIEW_PARSER_ARRAY_ENTRY& a2) noexcept
+	{
+		return addin::compareSmartViewParser(reinterpret_cast<LPCVOID>(&a1), reinterpret_cast<LPCVOID>(&a2));
 	}
 
 	TEST_CLASS(addinTest)
@@ -61,6 +73,60 @@ namespace addintest
 
 			// Same name compares equal
 			Assert::AreEqual(0, testCompareTags({1, 0, L"one"}, {2, 0, L"one"}));
+		}
+
+		TEST_METHOD(Test_compareNameID)
+		{
+			Assert::AreEqual(
+				0,
+				testCompareNameID(
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"},
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"}));
+			Assert::AreEqual(
+				-1,
+				testCompareNameID(
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"},
+					{2, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"}));
+			Assert::AreEqual(
+				1,
+				testCompareNameID(
+					{2, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"},
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"}));
+			Assert::AreEqual(
+				-1,
+				testCompareNameID(
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"},
+					{1, &guid::PSETID_Note, L"a", PT_SYSTIME, L"aa"}));
+			Assert::AreEqual(
+				-1,
+				testCompareNameID(
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"},
+					{1, &guid::PSETID_Meeting, L"b", PT_SYSTIME, L"bb"}));
+			Assert::AreEqual(
+				1,
+				testCompareNameID(
+					{1, &guid::PSETID_Meeting, L"b", PT_SYSTIME, L"bb"},
+					{1, &guid::PSETID_Meeting, L"a", PT_SYSTIME, L"aa"}));
+		}
+
+		TEST_METHOD(Test_CompareSmartViewParser)
+		{
+			Assert::AreEqual(
+				0, testCompareSmartViewParser({1, parserType::REPORTTAG, false}, {1, parserType::REPORTTAG, false}));
+			Assert::AreEqual(
+				-1, testCompareSmartViewParser({1, parserType::REPORTTAG, false}, {2, parserType::REPORTTAG, false}));
+			Assert::AreEqual(
+				1, testCompareSmartViewParser({2, parserType::REPORTTAG, false}, {1, parserType::REPORTTAG, false}));
+
+			Assert::AreEqual(
+				-1, testCompareSmartViewParser({1, parserType::ENTRYID, false}, {1, parserType::REPORTTAG, false}));
+			Assert::AreEqual(
+				1, testCompareSmartViewParser({1, parserType::REPORTTAG, false}, {1, parserType::ENTRYID, false}));
+
+			Assert::AreEqual(
+				-1, testCompareSmartViewParser({1, parserType::REPORTTAG, false}, {1, parserType::REPORTTAG, true}));
+			Assert::AreEqual(
+				1, testCompareSmartViewParser({1, parserType::REPORTTAG, true}, {1, parserType::REPORTTAG, false}));
 		}
 	};
 } // namespace addintest
