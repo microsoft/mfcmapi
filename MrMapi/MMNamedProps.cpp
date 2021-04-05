65438a70b144189a2732e06d0326ee8153fc8a48
Brad Hughes <bradhugh@microsoft.com>
2021-04-05 18:26:46 -0400

    Analyze excessive named property usage

--- MrMapi/MMNamedProps.cpp
+++ MrMapi/MMNamedProps.cpp
@@ -11,6 +11,8 @@
 #include <core/mapi/cache/namedProps.h>
 #include <core/addin/mfcmapi.h>
 #include <chrono>
+#include <unordered_set>
+#include <core/interpret/guid.h>
 
 template<typename... Arguments>
 void WriteOutput(FILE* fOut, LPCWSTR szMsg, Arguments&&... args)
@@ -42,6 +44,101 @@ void PrintGuid(FILE* fOut, const GUID& lpGuid)
 		lpGuid.Data4[5],
 		lpGuid.Data4[6],
 		lpGuid.Data4[7]);
+}
+
+class GuidHasher
+{
+public:
+	size_t operator()(const GUID& guid) const
+	{
+		// TODO: Figure out how not to take a depdendency on RPC
+		RPC_STATUS status;
+		std::uint16_t hash = UuidHash(const_cast<UUID*>(&guid), &status);
+		return std::hash<std::uint16_t>()(hash);
+	}
+};
+
+class GuidLess
+{
+public:
+	bool operator()(const GUID& lhs, const GUID& rhs) const
+	{
+		return memcmp(&lhs, &rhs, sizeof(GUID)) < 0;
+	}
+};
+
+void AnalyzeNamedProps(std::vector<std::shared_ptr<cache::namedPropCacheEntry>> const& nameToNumberMap)
+{
+	const long namedPropsQuota = 16384;
+	const std::uint32_t NamedPropsLeakPatternCheckPercentageThreshold = 30;
+	const std::uint32_t NamedPropsLeakPatternGuidPercentageThreshold = 20;
+	const std::uint32_t NamedPropsLeakPatternGuidPercentageThresholdForInternetHeaders = 60;
+	const std::uint32_t NamedPropsLeakPatternNamePrefixLength = 10;
+	const std::uint32_t NamedPropsLeakPatternNamePrefixPercentageThreshold = 10;
+
+	std::unordered_set<GUID, GuidHasher> leakPatternGuids;
+	std::unordered_set<std::wstring> leakPatternNamePrefixes;
+
+	// Simple protection from some bogus application which cause named property exhaustion.
+	// This protection is not bullet-proof, it just attempts to recognize two known bogus patterns.
+	// If bogus application doesn't follow any of those two patterns (e.g. just registers random names with random guids)
+	// then no luck for such mailbox - it will hit the quota and should be dealt with manually.
+	if constexpr (true || nameToNumberMap.size() > namedPropsQuota / 100 * NamedPropsLeakPatternCheckPercentageThreshold)
+	{
+		std::map<GUID, int, GuidLess> guidCounts;
+		std::map<std::wstring, int> namePrefixCounts;
+
+		// Loop through all properties and discovery leaky GUID patterns and leaky prefixes
+		for (auto &entry : nameToNumberMap)
+		{
+			auto registeredPropName = entry->getMapiNameId();
+			bool isInternetHeadersNamespace = memcmp(registeredPropName->lpguid, &guid::PS_INTERNET_HEADERS, sizeof(GUID)) == 0;
+
+			// Check for a GUID pattern. We consider abnormal if we see more than
+			// 2K names within the same GUID namespace. We allow larger limit for InternetHeaders
+			// because some mailboxes migrated from E14 have more properties in this namespace.
+			int count = guidCounts[*registeredPropName->lpguid];
+			guidCounts[*registeredPropName->lpguid] = ++count;
+
+			if ((isInternetHeadersNamespace && count > namedPropsQuota / 100 * NamedPropsLeakPatternGuidPercentageThresholdForInternetHeaders)
+				|| (false == isInternetHeadersNamespace && count > namedPropsQuota / 100 * NamedPropsLeakPatternGuidPercentageThreshold))
+			{
+				leakPatternGuids.emplace(*registeredPropName->lpguid);
+			}
+
+			// If this is a string name and name is long enough, check for a name prefix pattern. We consider abnormal if we see more than
+			// 1K names with the same 10-character prefix.
+			if (registeredPropName->ulKind == MNID_STRING && registeredPropName->Kind.lpwstrName != nullptr)
+			{
+				std::wstring namePrefix = registeredPropName->Kind.lpwstrName;
+				if (namePrefix.size() > NamedPropsLeakPatternNamePrefixLength)
+				{
+					namePrefix = namePrefix.substr(0, NamedPropsLeakPatternNamePrefixLength);
+				}
+
+				count = namePrefixCounts[namePrefix];
+				namePrefixCounts[namePrefix] = ++count;
+
+				if (count > namedPropsQuota / 100 * NamedPropsLeakPatternNamePrefixPercentageThreshold)
+				{
+					leakPatternNamePrefixes.emplace(namePrefix);
+				}
+			}
+		}
+	}
+
+	wprintf(L"Detected %zu leaky GUIDs\n", leakPatternGuids.size());
+	for (auto &guid : leakPatternGuids)
+	{
+		auto guidString = guid::GUIDToStringAndName(guid);
+		wprintf(L"%ws\n", guidString.c_str());
+	}
+
+	wprintf(L"Detected %zu leaky Prefixes\n", leakPatternNamePrefixes.size());
+	for (auto &prefix : leakPatternNamePrefixes)
+	{
+		wprintf(L"%ws\n", prefix.c_str());
+	}
 }
 
 void DoNamedProps(_In_opt_ LPMAPISESSION lpSession, _In_opt_ LPMDB lpMDB)
@@ -128,6 +225,8 @@ void DoNamedProps(_In_opt_ LPMAPISESSION lpSession, _In_opt_ LPMDB lpMDB)
 			break;
 		}
 	}
+
+	AnalyzeNamedProps(names);
 
 	wprintf(L"Completed.\n");
 	if (fOut) fclose(fOut);
