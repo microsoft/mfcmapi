e26cd96f61d6ab631971ae262d6a8683894a3ba3
Stephen Griffin <sgriffin@outlook.com>
2023-06-23 09:18:08 -0400

    Remove unreferenced var

--- core/smartview/SPropValueStruct.cpp
+++ core/smartview/SPropValueStruct.cpp
@@ -7,7 +7,7 @@ namespace smartview
 {
 	void SPropValueStruct::parse()
 	{
-		const auto ulCurrOffset = parser->getOffset();
+		const auto ulCurrOffset = parser->getOffset(); 
 
 		PropType = blockT<WORD>::parse(parser);
 		PropID = blockT<WORD>::parse(parser);
