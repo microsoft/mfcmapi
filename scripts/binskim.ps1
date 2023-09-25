3889d24f7be2aa58208466550073ff6e3ee0e1a0
Stephen Griffin <sgriffin@outlook.com>
2023-09-25 18:01:13 -0400

    fix up binskim results

---
+++ scripts/binskim.ps1
@@ -0,0 +1,13 @@
+binskim.exe analyze C:\src\mfcmapi\MFCMapi.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\MrMAPI\Release\MrMAPI.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\MrMAPI\Release_Unicode\MrMAPI.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\Release\MFCMapi.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\Release_Unicode\MFCMapi.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\exampleMapiConsoleApp\Release\exampleMapiConsoleApp.exe
+binskim.exe analyze C:\src\mfcmapi\bin\Win32\exampleMapiConsoleApp\Release_Unicode\exampleMapiConsoleApp.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\MrMAPI\Release\MrMAPI.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\MrMAPI\Release_Unicode\MrMAPI.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\Release\MFCMapi.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\Release_Unicode\MFCMapi.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\exampleMapiConsoleApp\Release\exampleMapiConsoleApp.exe
+binskim.exe analyze C:\src\mfcmapi\bin\x64\exampleMapiConsoleApp\Release_Unicode\exampleMapiConsoleApp.exe
