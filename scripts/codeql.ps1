3c5c30de7d4e3da76b614e6191d53c56189675ab
Stephen Griffin <sgriffin@outlook.com>
2023-10-13 16:33:23 -0400

    Local codeql working now

--- scripts/codeql.ps1
+++ scripts/codeql.ps1
@@ -1,15 +1,33 @@
 # Define the installation directory as a subdir of scripts
-$codeqlRoot = Join-Path -Path $PSScriptRoot -ChildPath "codeql"
+$scriptroot = $PSScriptRoot
+$codeqlRoot = Join-Path -Path $scriptroot -ChildPath "codeql"
+# src root is parent of scripts root
+$srcRoot = Join-Path -Path $scriptroot -ChildPath ".."
 $codeql = Join-Path $codeqlRoot "codeql.exe"
+$db = Join-Path $env:TEMP "codeql_databases"
+
+Write-Host "CodeQL root: $codeqlRoot"
+Write-Host "Source root: $srcRoot"
+Write-Host "CodeQL: $codeql"
 
 # if codeql.exe does not exist, install it by running install-codeql.ps1
 if (-not (Test-Path -Path "$codeqlRoot\codeql.exe")) {
     Write-Host "CodeQL is not installed. Installing..."
-    & $PSScriptRoot\install-codeql.ps1
+    & $scriptroot\install-codeql.ps1
 }
 
 # Verify installation by displaying the CodeQL version
 & $codeql --version
 
-& $codeql resolve languages --format=betterjson --extractor-options-verbosity=4 --extractor-include-aliases
-& $codeql resolve queries cpp-code-scanning.qls --format=bylanguage
+# & $codeql resolve languages --format=betterjson --extractor-options-verbosity=4 --extractor-include-aliases
+# & $codeql resolve queries cpp-code-scanning.qls --format=bylanguage
+
+Write-Host "Creating database..."
+if (Test-Path -Path $db) {
+    Remove-Item -Path $db -Recurse -Force
+}
+
+& $codeql database create $db --overwrite --language=cpp --command="powershell $scriptroot\build.ps1"
+
+Write-Host "Running analysis..."
+& $codeql database analyze $db --threads=4 -v --format sarif-latest -o $scriptroot\codeql.sarif
