73ef84429654336e089c0251e95257f5a35c01a5
Stephen Griffin <sgriffin@outlook.com>
2024-06-17 14:01:50 -0400

    try different clang runner

--- scripts/clang.ps1
+++ scripts/clang.ps1
@@ -1,26 +1,28 @@
 $vsRoot = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
 $vcInstallDir = Join-Path $vsRoot "VC"
-$clangdir = Join-Path $vcInstallDir "Tools\Llvm\bin\clang-format.exe"
+$clang = Join-Path $vcInstallDir "Tools\Llvm\bin\clang-format.exe"
 
 # Check if VC install directory was found
 if ($null -eq $vcInstallDir) {
     Write-Host "Visual C++ installation directory not found."
 }
 
-if ($null -eq $clangdir) {
+if ($null -eq $clang) {
     Write-Host "clang not found."
 }
+Write-Host "clang-format found at $clang"
+& $clang --version
 
 Push-Location ..
 
 Write-Host "Formatting C++ headers"
 Get-ChildItem -Recurse -Filter *.h | Where-Object { $_.DirectoryName -notlike "*include*" } | ForEach-Object {
-    & $clangdir -i $_.FullName
+    & $clang -i $_.FullName
 }
 
 Write-Host "Formatting C++ sources"
 Get-ChildItem -Recurse -Filter *.cpp | ForEach-Object {
-    & $clangdir -i $_.FullName
+    & $clang -i $_.FullName
 }
 
 Pop-Location
