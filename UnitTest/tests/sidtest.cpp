f067c83c1c703336a00c18b2e6661f33e6ac8a7f
Stephen Griffin <sgriffin@outlook.com>
2025-01-16 14:04:32 -0500

    Remove dead code

--- UnitTest/tests/sidtest.cpp
+++ UnitTest/tests/sidtest.cpp
@@ -3,11 +3,6 @@
 #include <core/interpret/sid.h>
 #include <core/utility/strings.h>
 
-namespace sid
-{
-	std::wstring ACEToString(const std::vector<BYTE>& buf, aceType acetype);
-}
-
 namespace sidtest
 {
 	TEST_CLASS(sidtest)
@@ -18,32 +13,22 @@ namespace sidtest
 
 		TEST_CLASS_INITIALIZE(initialize) { unittest::init(); }
 
-		TEST_METHOD(Test_GetTextualSid)
+		TEST_METHOD(Test_LookupAccountSid)
 		{
-			Assert::AreEqual(std::wstring{}, sid::GetTextualSid({}));
 			auto nullAccount = sid::LookupAccountSid({});
 			Assert::AreEqual(std::wstring{L"(no domain)"}, nullAccount.getDomain());
 			Assert::AreEqual(std::wstring{L"(no name)"}, nullAccount.getName());
 
-			Assert::AreEqual(std::wstring{}, sid::GetTextualSid({12}));
 			auto invalidAccount = sid::LookupAccountSid({12});
 			Assert::AreEqual(std::wstring{L"(no domain)"}, invalidAccount.getDomain());
 			Assert::AreEqual(std::wstring{L"(no name)"}, invalidAccount.getName());
 
 			auto simpleSidBin = strings::HexStringToBin(L"010500000000000515000000A065CF7E784B9B5FE77C8770091C0100");
-			Assert::AreEqual(
-				std::wstring{L"S-1-5-21-2127521184-1604012920-1887927527-72713"}, sid::GetTextualSid(simpleSidBin));
 			auto simpleSidAccount = sid::LookupAccountSid(simpleSidBin);
 			Assert::AreEqual(std::wstring{L"(no domain)"}, simpleSidAccount.getDomain());
 			Assert::AreEqual(std::wstring{L"(no name)"}, simpleSidAccount.getName());
 
-			Assert::AreEqual(
-				std::wstring{L"S-1-000102030405-21-2127521184-1604012920-1887927527-72713"},
-				sid::GetTextualSid(
-					strings::HexStringToBin(L"010500010203040515000000A065CF7E784B9B5FE77C8770091C0100")));
-
 			auto authenticatedUsersSidBin = strings::HexStringToBin(L"01 01 000000000005 0B000000");
-			Assert::AreEqual(std::wstring{L"S-1-5-11"}, sid::GetTextualSid(authenticatedUsersSidBin));
 			auto authenticatedUsersSidAccount = sid::LookupAccountSid(authenticatedUsersSidBin);
 			Assert::AreEqual(std::wstring{L"NT AUTHORITY"}, authenticatedUsersSidAccount.getDomain());
 			Assert::AreEqual(std::wstring{L"Authenticated Users"}, authenticatedUsersSidAccount.getName());
@@ -62,12 +47,6 @@ namespace sidtest
 			auto account2 = sid::SidAccount{std::wstring{L"foo"}, std::wstring{L"bar"}};
 			Assert::AreEqual(std::wstring{L"foo"}, account2.getDomain());
 			Assert::AreEqual(std::wstring{L"bar"}, account2.getName());
-		}
-
-		TEST_METHOD(Test_ACEToString)
-		{
-			// test ACEToString with a zero length vector
-			unittest::AreEqualEx(std::wstring{L""}, ACEToString({}, sid::aceType::Container));
 		}
 	};
 } // namespace sidtest
--- core/interpret/sid.h
+++ core/interpret/sid.h
@@ -31,8 +31,6 @@ namespace sid
 
 	_Check_return_ std::wstring LookupIdentifierAuthority(const SID_IDENTIFIER_AUTHORITY& authority);
 	_Check_return_ std::wstring IdentifierAuthorityToString(const SID_IDENTIFIER_AUTHORITY& authority);
-	_Check_return_ std::wstring GetTextualSid(_In_opt_ PSID pSid);
-	_Check_return_ std::wstring GetTextualSid(std::vector<BYTE> buf);
 	_Check_return_ SidAccount LookupAccountSid(PSID SidStart);
 	_Check_return_ SidAccount LookupAccountSid(std::vector<BYTE> buf);
 	_Check_return_ std::wstring NTSDToString(const std::vector<BYTE>& buf, aceType acetype);
